d22b6617063c844805389dd6b6f7f4720e5f76d5
Swift <swiftx37@gmail.com>
2023-07-22 20:29:38 +0200

    EditProduct action and page

--- OnlineStore/Controllers/AdminController.cs
+++ OnlineStore/Controllers/AdminController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Hosting;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.CodeAnalysis.FlowAnalysis;
 using Microsoft.EntityFrameworkCore;
 using OnlineStore.Data;
 using OnlineStore.Domain;
@@ -152,11 +154,11 @@ namespace OnlineStore.Controllers
                 model.ImageFile.Length > 0 && 
                 model.ImageFile.ContentType.Contains("image"))
             {
-                
-                var path = Path.Combine(_webHostEnvironment.ContentRootPath, "Resources", "ProductImages", model.ImageFile.FileName);
+                var relativePath = Path.Combine("resources", "productsImages", model.ImageFile.FileName);
+                var path = Path.Combine(_webHostEnvironment.WebRootPath, relativePath);
                 using (Stream stream = new FileStream(path, FileMode.Create))
                     model.ImageFile.CopyTo(stream);
-                product.Image = path;
+                product.Image = "\\" + relativePath;
             }
 
             _context.Entry(product).State = EntityState.Added;
@@ -164,22 +166,54 @@ namespace OnlineStore.Controllers
             return RedirectToAction("Categories");
         }
 
+        [HttpGet]
         public IActionResult EditProduct(int id)
         {
             var product = _context.Products.SingleOrDefault(p => p.Id == id);
             if (product is null) return RedirectToAction("NotFound", "Error");
 
-            var model = new ProductViewModel
-            {
-                Id = subcategory.Id,
-                Name = subcategory.Name,
-                CategoryId = subcategory.CategoryId,
-                AvailableCategories = new SelectList(
-        _context.Categories,
-        nameof(Category.Id),
-        nameof(Category.Name))
-            };
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<Product, ProductViewModel>());
+            var mapper = config.CreateMapper();
 
+            var model = mapper.Map<ProductViewModel>(product);
+            model.AvailableCategories = new SelectList(
+                _context.Categories,
+                nameof(Category.Id),
+                nameof(Category.Name));
+            model.AvailableSubCategories = new SelectList(
+                _context.SubCategories.Where(sc => sc.CategoryId == model.CategoryId),
+                nameof(SubCategory.Id),
+                nameof(SubCategory.Name));
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult EditProduct(ProductViewModel model)
+        {
+            var product = _context.Products.SingleOrDefault(c => c.Id == model.Id);
+
+            if (product is null) return RedirectToAction("NotFound", "Error");
+
+            var config = new MapperConfiguration(cfg => 
+            cfg.CreateMap<ProductViewModel, Product>().ForMember(p => p.Image, opt => opt.Ignore()));
+            var mapper = config.CreateMapper();
+
+            mapper.Map(model, product);
+
+            if (model.ImageFile != null &&
+                model.ImageFile.Length > 0 &&
+                model.ImageFile.ContentType.Contains("image"))
+            {
+                var relativePath = Path.Combine("resources", "productsImages", model.ImageFile.FileName);
+                var path = Path.Combine(_webHostEnvironment.WebRootPath, relativePath);
+                using (Stream stream = new FileStream(path, FileMode.Create))
+                    model.ImageFile.CopyTo(stream);
+                product.Image = "\\" + relativePath;
+            }
+
+            _context.Entry(product).State = EntityState.Modified;
+            _context.SaveChanges();
             return RedirectToAction("Categories");
         }
 
--- OnlineStore/Models/ProductViewModel.cs
+++ OnlineStore/Models/ProductViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using OnlineStore.Domain;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Runtime.InteropServices;
 
 namespace OnlineStore.Models
 {
