5b6efba80b1ee10c33a38243d2a1ee0c67676238
Swift <swiftx37@gmail.com>
2023-07-26 17:58:17 +0200

    Cart and CartItem entities

---
+++ OnlineStore.Domain/Cart.cs
@@ -0,0 +1,27 @@
+﻿using OnlineStore.Domain.Base;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OnlineStore.Domain
+{
+    public class Cart : Entity
+    {
+        public CartStatus Status { get; set; } = CartStatus.Active;
+
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+
+        public DateTime? PayDate { get; set; }
+
+        public IEnumerable<CartItem>? CartItems { get; set; }
+
+        [NotMapped]  
+        public decimal TotalPrice => CartItems?.Sum(i => i.Product?.UnitPrice * i.Quantity) ?? 0;
+
+        [NotMapped]
+        public int TotalQuantity => CartItems?.Sum(i => i.Quantity) ?? 0;
+    }
+
+    public enum CartStatus
+    {
+        Completed, Active
+    }
+}
--- OnlineStore/Data/ApplicationDbContext.cs
+++ OnlineStore/Data/ApplicationDbContext.cs
@@ -11,7 +11,8 @@ namespace OnlineStore.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<SubCategory> SubCategories { get; set; }
-
         public DbSet<ProductDetails> ProductDetails { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }  
     }
 }
