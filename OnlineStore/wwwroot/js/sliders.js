0c4ba1b75e14038c7e80577bb50e5085920408ff
Swift <swiftx37@gmail.com>
2023-08-12 18:58:32 +0200

    Slider for index page

---
+++ OnlineStore/wwwroot/js/sliders.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper(".main-slaider", {
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+    },
+    autoplay: {
+        delay: 10000,
+        disableOnInteraction: false,
+    },
+    loop: true,
+    speed: 500
+});
