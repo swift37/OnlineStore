66d356826ec9f1ec67891f91f075fbb4657dbccd
Swift <swiftx37@gmail.com>
2023-08-28 16:24:05 +0200

    WishlistStatusViewComponent

---
+++ OnlineStore/ViewComponents/WishlistStatusViewComponent.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Data;
+
+namespace OnlineStore.ViewComponents
+{
+    public class WishlistStatusViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public WishlistStatusViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var wishlist = await _context.Wishlists
+                .Include(c => c.Products)
+                .FirstOrDefaultAsync();
+
+            ViewBag.WishlistItems = wishlist?.TotalQuantity ?? 0;
+
+            return View();
+        }
+    }
+}
