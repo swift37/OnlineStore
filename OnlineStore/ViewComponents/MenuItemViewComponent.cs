ca2184d9c1db6371179fe826ff8c00cbebb5288a
Swift <swiftx37@gmail.com>
2023-11-09 16:52:00 +0100

    Adding the MenuItemGroup and MenuItem view components.

---
+++ OnlineStore/ViewComponents/MenuItemViewComponent.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Domain;
+
+namespace OnlineStore.ViewComponents
+{
+    public class MenuItemViewComponent : ViewComponent
+    {
+        public Task<IViewComponentResult> InvokeAsync(MenuItem menuItem) => 
+            Task.FromResult<IViewComponentResult>(View(menuItem));
+    }
+}
---
+++ OnlineStore/ViewComponents/MenuItemsGroupViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Data;
+
+namespace OnlineStore.ViewComponents
+{
+    public class MenuItemsGroupViewComponent : ViewComponent
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MenuItemsGroupViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var menuGroup = await _context.MenuItems
+                .Include(i => i.Category)
+                .Include(i => i.Categories)
+                .ToArrayAsync();
+
+            return View(menuGroup);
+        }
+    }
+}
