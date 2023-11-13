ce4a5e5069326fa29c7c8c52ead3be43cbe916c2
Swift <swiftx37@gmail.com>
2023-11-13 11:50:46 +0100

    Creating the generic repository interface.

---
+++ OnlineStore.Interfaces/Repositories/IRepository.cs
@@ -0,0 +1,20 @@
+﻿using OnlineStore.Interfaces.Entities;
+
+namespace OnlineStore.Interfaces.Repositories
+{
+    public interface IRepository<T> where T : IEntity
+    {
+        Task<IEnumerable<T>> GetAll();
+
+        Task<bool> Exists(int id);
+
+        Task<T> Get(int id);
+
+        Task<T> Create(T entity);
+
+        Task Update(T entity);
+
+        Task Delete(T entity);
+
+    }
+}
