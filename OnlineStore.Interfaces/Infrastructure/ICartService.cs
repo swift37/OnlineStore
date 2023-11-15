07299acf082252079efebe1a4a2c0c686a190aa7
Swift <swiftx37@gmail.com>
2023-11-15 21:33:07 +0100

    Creating CartStore and CartService interfaces.

---
+++ OnlineStore.Interfaces/Infrastructure/ICartService.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineStore.Interfaces.Infrastructure
+{
+    public interface ICartService
+    {
+        void Add(int id);
+
+        void Decrement(int id);
+
+        void Remove(int id);
+
+        void Clear();
+    }
+}
---
+++ OnlineStore.Interfaces/Infrastructure/ICartStore.cs
@@ -0,0 +1,9 @@
+﻿using OnlineStore.Domain;
+
+namespace OnlineStore.Interfaces.Infrastructure
+{
+    public interface ICartStore
+    {
+        Cart? Cart { get; set; }
+    }
+}
