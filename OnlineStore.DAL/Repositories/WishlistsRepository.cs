1d2b902f7dbfa11d3b87078763d699926122af38
Swift <swiftx37@gmail.com>
2023-11-17 20:41:10 +0100

    Implementing the wishlists repository.

---
+++ OnlineStore.DAL/Repositories/WishlistsRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Application.Interfaces.Repositories;
+using OnlineStore.DAL.Context;
+using OnlineStore.Domain;
+
+namespace OnlineStore.DAL.Repositories
+{
+    public class WishlistsRepository : Repository<Wishlist>, IWishlistsRepository
+    {
+        public WishlistsRepository(ApplicationDbContext context) : base(context) { }
+
+        public async Task<IEnumerable<Wishlist>> GetUserWishlistAsync(Guid userId) => await DbSet
+            .Where(o => o.UserId == userId)
+            .ToArrayAsync();
+    }
+}
