f9f0386edbd044cd051647dd70dff34365924375
Swift <swiftx37@gmail.com>
2023-11-17 19:54:04 +0100

    Adding ContactRequest and Subscribe DTOs.

---
+++ OnlineStore.Application/DTOs/ContactRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace OnlineStore.Application.DTOs
+{
+    public class ContactRequestDTO
+    {
+        public int Id {  get; set; }
+
+        public string? ContactName { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Message { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime ResponseDate { get; set; }
+    }
+}
---
+++ OnlineStore.Application/DTOs/SubscriberDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace OnlineStore.Application.DTOs
+{
+    public class SubscriberDTO
+    {
+        public int Id { get; set; }
+
+        public string? Email { get; set; }
+
+        public DateTime SubscribeDate { get; set; }
+    }
+}
