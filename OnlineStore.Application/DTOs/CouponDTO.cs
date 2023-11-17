d435e9f3607ecad58644b0c48f385c5057cb1259
Swift <swiftx37@gmail.com>
2023-11-17 19:47:01 +0100

    Adding the Coupon DTO.

---
+++ OnlineStore.Application/DTOs/CouponDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace OnlineStore.Application.DTOs
+{
+    public class CouponDTO
+    {
+        public int Id { get; set; }
+
+        public string? Number { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime? FinishDate { get; set; }
+
+        public decimal DiscountSize { get; set; }
+
+        public double PercentDiscountSize { get; set; }
+
+        public int MaxUsesCount { get; set; }
+
+        public int CurrentUsesCount { get; set; }
+
+        public bool IsNotUsesLimit { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
