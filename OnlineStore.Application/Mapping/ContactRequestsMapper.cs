9905b22065b5f8b7492d463ae57a866e6b5a7d65
Swift <swiftx37@gmail.com>
2023-11-17 20:07:10 +0100

    Creating the ContactRequestsMapper and SubscribersMapper.

---
+++ OnlineStore.Application/Mapping/ContactRequestsMapper.cs
@@ -0,0 +1,32 @@
+﻿using OnlineStore.Application.DTOs;
+using OnlineStore.Domain;
+
+namespace OnlineStore.Application.Mapping
+{
+    public static class ContactRequestsMapper
+    {
+        public static ContactRequestDTO ToDTO(this ContactRequest contactRequest) => new ContactRequestDTO
+        {
+            Id = contactRequest.Id,
+            ContactName = contactRequest.ContactName,
+            Email = contactRequest.Email,
+            Message = contactRequest.Message,
+            CreationDate = contactRequest.CreationDate,
+            ResponseDate = contactRequest.ResponseDate
+        };
+
+        public static ContactRequest FromDTO(this ContactRequestDTO contactRequest) => new ContactRequest
+        {
+            Id = contactRequest.Id,
+            ContactName = contactRequest.ContactName,
+            Email = contactRequest.Email,
+            Message = contactRequest.Message,
+            CreationDate = contactRequest.CreationDate,
+            ResponseDate = contactRequest.ResponseDate
+        };
+
+        public static IEnumerable<ContactRequestDTO> ToDTO(this IEnumerable<ContactRequest> contactRequests) => contactRequests.Select(c => c.ToDTO());
+
+        public static IEnumerable<ContactRequest> FromDTO(this IEnumerable<ContactRequestDTO> contactRequests) => contactRequests.Select(c => c.FromDTO());
+    }
+}
---
+++ OnlineStore.Application/Mapping/SubscribersMapper.cs
@@ -0,0 +1,31 @@
+﻿using OnlineStore.Application.DTOs;
+using OnlineStore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Application.Mapping
+{
+    public static class SubscribersMapper
+    {
+        public static SubscriberDTO ToDTO(this Subscriber subscriber) => new SubscriberDTO
+        {
+            Id = subscriber.Id,
+            Email = subscriber.Email,
+            SubscribeDate = subscriber.SubscribeDate
+        };
+
+        public static Subscriber FromDTO(this SubscriberDTO subscriber) => new Subscriber
+        {
+            Id = subscriber.Id,
+            Email = subscriber.Email,
+            SubscribeDate = subscriber.SubscribeDate
+        };
+
+        public static IEnumerable<SubscriberDTO> ToDTO(this IEnumerable<Subscriber> subscribers) => subscribers.Select(c => c.ToDTO());
+
+        public static IEnumerable<Subscriber> FromDTO(this IEnumerable<SubscriberDTO> subscribers) => subscribers.Select(c => c.FromDTO());
+    }
+}
