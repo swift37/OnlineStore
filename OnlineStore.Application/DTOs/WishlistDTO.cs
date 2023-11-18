8449efe2d775d52c30c656c89f03e53931651165
Swift <swiftx37@gmail.com>
2023-11-18 17:43:41 +0100

    Adding wishlist DTO and wishlists mapper.

---
+++ OnlineStore.Application/DTOs/WishlistDTO.cs
@@ -0,0 +1,17 @@
+﻿using OnlineStore.Domain;
+
+namespace OnlineStore.Application.DTOs
+{
+    public class WishlistDTO
+    {
+        public int Id { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public DateTime CreateDate { get; set; }
+
+        public DateTime LastChangeDate { get; set; }
+
+        public ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    }
+}
---
+++ OnlineStore.Application/Mapping/WishlistsMapper.cs
@@ -0,0 +1,30 @@
+﻿using OnlineStore.Application.DTOs;
+using OnlineStore.Domain;
+
+namespace OnlineStore.Application.Mapping
+{
+    public static class WishlistsMapper
+    {
+        public static WishlistDTO ToDTO(this Wishlist wishlist) => new WishlistDTO
+        {
+            Id = wishlist.Id,
+            UserId = wishlist.UserId,
+            CreateDate = wishlist.CreateDate,
+            LastChangeDate = wishlist.LastChangeDate,
+            Products = wishlist.Products
+        };
+
+        public static Wishlist FromDTO(this WishlistDTO wishlist) => new Wishlist
+        {
+            Id = wishlist.Id,
+            UserId = wishlist.UserId,
+            CreateDate = wishlist.CreateDate,
+            LastChangeDate = wishlist.LastChangeDate,
+            Products = wishlist.Products
+        };
+
+        public static IEnumerable<WishlistDTO> ToDTO(this IEnumerable<Wishlist> wishlists) => wishlists.Select(p => p.ToDTO());
+
+        public static IEnumerable<Wishlist> FromDTO(this IEnumerable<WishlistDTO> wishlists) => wishlists.Select(p => p.FromDTO());
+    }
+}
