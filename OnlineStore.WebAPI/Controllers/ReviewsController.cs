9971435e33ffc314a5afaf85f5c5638cd07d49de
Swift <swiftx37@gmail.com>
2023-11-18 16:25:28 +0100

    Creating the reviews controller.

--- OnlineStore.WebAPI/Controllers/ReviewsController.cs
+++ OnlineStore.WebAPI/Controllers/ReviewsController.cs
@@ -2,40 +2,41 @@
 using OnlineStore.Application.DTOs;
 using OnlineStore.Application.Interfaces.Repositories;
 using OnlineStore.Application.Mapping;
+using OnlineStore.Domain;
 using OnlineStore.WebAPI.Controllers.Base;
 
 namespace OnlineStore.WebAPI.Controllers
 {
     [Produces("application/json")]
-    public class ProductsController : BaseController
+    public class ReviewsController : BaseController
     {
-        private readonly IProductsRepository _repository;
+        private readonly IReviewsRepository _repository;
 
-        public ProductsController(IProductsRepository repository) => 
+        public ReviewsController(IReviewsRepository repository) =>
             _repository = repository;
 
         /// <summary>
-        /// Get the enumeration of products
+        /// Get the enumeration of reviews
         /// </summary>
         /// <remarks>
         /// Sample request:
-        /// GET /products
+        /// GET /reviews
         /// </remarks>
-        /// <returns>Returns IEnumerable<ProductDTO></returns>
+        /// <returns>Returns IEnumerable<ReviewDTO></returns>
         /// <response code="200">Success</response>
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<ActionResult<IEnumerable<ProductDTO>>> GetAll() => 
+        public async Task<ActionResult<IEnumerable<ReviewDTO>>> GetAll() =>
             Ok((await _repository.GetAllAsync()).ToDTO());
 
         /// <summary>
-        /// Get true if product exists
+        /// Get true if review exists
         /// </summary>
         /// <remarks>
         /// Sample request:
-        /// GET /products/exists/1
+        /// GET /reviews/exists/1
         /// </remarks>
-        /// <param name="id">Product id</param>
+        /// <param name="id">Review id</param>
         /// <returns>Returns bool</returns>
         /// <response code="200">Success</response>
         /// <response code="404">Not Found</response>
@@ -46,78 +47,78 @@ namespace OnlineStore.WebAPI.Controllers
             await _repository.ExistsAsync(id) ? Ok(true) : NotFound(false);
 
         /// <summary>
-        /// Get the product by id
+        /// Get the review by id
         /// </summary>
         /// <remarks>
         /// Sample request:
-        /// GET /products/1
+        /// GET /reviews/1
         /// </remarks>
-        /// <param name="id">Product id (int)</param>
-        /// <returns>Returns ProductDTO</returns>
+        /// <param name="id">Review id (int)</param>
+        /// <returns>Returns ReviewDTO</returns>
         /// <response code="200">Success</response>
         /// <response code="404">Not Found</response>
         [HttpGet("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<ProductDTO>> Get(int id)
+        public async Task<ActionResult<ReviewDTO>> Get(int id)
         {
-            var product = await _repository.GetAsync(id);
-            if (product is null) return NotFound();
+            var review = await _repository.GetAsync(id);
+            if (review is null) return NotFound();
 
-            return Ok(product.ToDTO());
+            return Ok(review.ToDTO());
         }
 
         /// <summary>
-        /// Create a product
+        /// Create a review
         /// </summary>
         /// <remarks>
-        /// POST /products
+        /// POST /reviews
         /// {
-        ///     name: "Product name",
+        ///     name: "Review name",
         ///     price: "2155"
         /// }
         /// </remarks>
-        /// <param name="productDTO">ProductDTO</param>
+        /// <param name="reviewDTO">ReviewDTO</param>
         /// <returns>Returns entity id</returns>
         /// <response code="200">Success</response>
-        /// <response code="422">If the incorrect product DTO was passed</response>
+        /// <response code="422">If the incorrect review DTO was passed</response>
         [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
-        public async Task<ActionResult<int>> Create([FromBody] ProductDTO productDTO)
+        public async Task<ActionResult<int>> Create([FromBody] ReviewDTO reviewDTO)
         {
-            var product = await _repository.CreateAsync(productDTO.FromDTO());
-            if (product is null) return UnprocessableEntity();
-            return Ok(product.Id);
+            var review = await _repository.CreateAsync(reviewDTO.FromDTO());
+            if (review is null) return UnprocessableEntity();
+            return Ok(review.Id);
         }
 
         /// <summary>
-        /// Update the product
+        /// Update the review
         /// </summary>
         /// <remarks>
-        /// PUT /products
+        /// PUT /reviews
         /// {
-        ///     name: "Updated product name"
+        ///     name: "Updated review name"
         /// }
         /// </remarks>
-        /// <param name="productDTO">ProductDTO</param>
+        /// <param name="reviewDTO">ReviewDTO</param>
         /// <returns>Returns NoContent</returns>
         /// <response code="204">Success</response>
         [HttpPut]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
-        public async Task<IActionResult> Update([FromBody] ProductDTO productDTO)
+        public async Task<IActionResult> Update([FromBody] ReviewDTO reviewDTO)
         {
-            await _repository.UpdateAsync(productDTO.FromDTO());
+            await _repository.UpdateAsync(reviewDTO.FromDTO());
             return NoContent();
         }
 
         /// <summary>
-        /// Delete the product by id
+        /// Delete the review by id
         /// </summary>
         /// <remarks>
-        /// DELETE /products/1
+        /// DELETE /reviews/1
         /// </remarks>
-        /// <param name="id">Product id (int)</param>
+        /// <param name="id">Review id (int)</param>
         /// <returns>Returns NoContent</returns>
         /// <response code="204">Success</response>
         /// <response code="404">Not Found</response>
@@ -128,33 +129,27 @@ namespace OnlineStore.WebAPI.Controllers
             await _repository.DeleteAsync(id) ? NoContent() : NotFound();
 
         /// <summary>
-        /// Get the products page by category id
+        /// Get the reviews page by category id
         /// </summary>
         /// <remarks>
-        /// GET /products/page?categoryId=1&page=3&itemsPerPage=20
+        /// GET /reviews/page?categoryId=1&page=3&itemsPerPage=20
         /// </remarks>
         /// <param name="categoryId">Category id (int)</param>
         /// <param name="page">Page number</param>
         /// <param name="itemsPerPage">Number of items per page</param>
         /// <param name="sortBy">Sort by statement</param>
-        /// <returns>Returns ProductsPageDTO</returns>
+        /// <returns>Returns IEnumerable<ReviewDTO></returns>
         /// <response code="200">Success</response>
-        [HttpGet("page")]
+        [HttpGet("product/{productid:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<ActionResult<ProductsPageDTO>> GetProductsByCategory(
-            int categoryId, 
-            int page = 1, 
-            int itemsPerPage = 15, 
-            IProductsRepository.SortParameters sortBy = IProductsRepository.SortParameters.Default)
+        public async Task<ActionResult<IEnumerable<ReviewDTO>>> GetReviewsByProduct(int productId)
         {
-            var prodcutsPage = await _repository
-                .GetProductsByCategoryAsync(categoryId, page, itemsPerPage, sortBy);
+            var reviews = await _repository.GetReviewsByProductAsync(productId);
 
-            if (prodcutsPage == null) return NotFound();
+            if (reviews == null || reviews.Count() < 1) return NotFound();
 
-            return Ok(prodcutsPage.ToDTO());
+            return Ok(reviews.ToDTO());
         }
-
     }
 }
