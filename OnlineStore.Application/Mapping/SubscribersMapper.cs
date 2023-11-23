2dfc328ec233dda816b77acd5403dbce1912d53e
Swift <swiftx37@gmail.com>
2023-11-23 17:44:19 +0100

    Updating the subscribers mapper.

--- OnlineStore.Application/Mapping/SubscribersMapper.cs
+++ OnlineStore.Application/Mapping/SubscribersMapper.cs
@@ -9,27 +9,31 @@ namespace OnlineStore.Application.Mapping
         {
             Id = subscriber.Id,
             Email = subscriber.Email,
-            SubscribeDate = subscriber.SubscribeDate
+            SubscribeDate = subscriber.SubscribeDate,
+            IsActive = subscriber.IsActive
         };
 
         public static Subscriber FromDTO(this SubscriberDTO subscriber) => new Subscriber
         {
             Id = subscriber.Id,
             Email = subscriber.Email,
-            SubscribeDate = subscriber.SubscribeDate
+            SubscribeDate = subscriber.SubscribeDate,
+            IsActive = subscriber.IsActive
         };
 
         public static Subscriber FromDTO(this CreateSubscriberDTO subscriber) => new Subscriber
         {
             Email = subscriber.Email,
-            SubscribeDate = subscriber.SubscribeDate
+            SubscribeDate = subscriber.SubscribeDate,
+            IsActive = subscriber.IsActive
         };
 
         public static Subscriber FromDTO(this UpdateSubscriberDTO subscriber) => new Subscriber
         {
             Id = subscriber.Id,
             Email = subscriber.Email,
-            SubscribeDate = subscriber.SubscribeDate
+            SubscribeDate = subscriber.SubscribeDate,
+            IsActive = subscriber.IsActive
         };
 
         public static IEnumerable<SubscriberDTO> ToDTO(this IEnumerable<Subscriber> subscribers) => subscribers.Select(c => c.ToDTO());
