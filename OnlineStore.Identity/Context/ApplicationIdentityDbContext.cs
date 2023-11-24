ae6daaff3a518ba8dba10df9cfdf7f8d72852f0c
Swift <swiftx37@gmail.com>
2023-11-24 20:47:32 +0100

    Creating the application identity database context.

---
+++ OnlineStore.Identity/Context/ApplicationIdentityDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using OnlineStore.Identity.Models;
+
+namespace OnlineStore.Identity.Context
+{
+    public class ApplicationIdentityDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options) 
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
