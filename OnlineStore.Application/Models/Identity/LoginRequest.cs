385f9f2865599edd303ff3b2166905002d8bca79
Swift <swiftx37@gmail.com>
2023-11-25 20:31:10 +0100

    Creating the login request and register request models.

---
+++ OnlineStore.Application/Models/Identity/LoginRequest.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.Application.Models.Identity
+{
+    public class LoginRequest
+    {
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+    }
+}
---
+++ OnlineStore.Application/Models/Identity/RegisterRequest.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.Application.Models.Identity
+{
+    internal class RegisterRequest
+    {
+        [Required]
+        public string? FirstName { get; set; }
+
+        [Required]
+        public string? LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string? PasswordConfirmation { get; set; }
+    }
+}
