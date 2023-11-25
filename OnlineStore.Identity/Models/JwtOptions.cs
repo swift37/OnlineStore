1d98c9ce9f35fd05bf12ac4101ec485fc5f7feb6
Swift <swiftx37@gmail.com>
2023-11-25 20:32:43 +0100

    Jwt options model.

---
+++ OnlineStore.Identity/Models/JwtOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineStore.Identity.Models
+{
+    public class JwtOptions
+    {
+        public string SecretKey { get; init; }
+
+        public string Issuer { get; init; }
+
+        public string Audience { get; init; }
+
+        public double DurationInMinutes { get; init; }
+    }
+}
