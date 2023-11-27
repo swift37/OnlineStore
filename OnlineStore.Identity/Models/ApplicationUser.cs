71398522745e7f914180739a0cf333ca66c3985a
Swift <swiftx37@gmail.com>
2023-11-27 19:31:15 +0100

    Extending the ApplicationUser model for using refresh tokens. Some fixes to make the application work correctly.

--- OnlineStore.Identity/Models/ApplicationUser.cs
+++ OnlineStore.Identity/Models/ApplicationUser.cs
@@ -7,5 +7,9 @@ namespace OnlineStore.Identity.Models
         public string? FirstName { get; set; }
 
         public string? LastName { get; set; }
+
+        public string? RefreshToken { get; set; }
+
+        public DateTime? RefreshTokenExpiry { get; set; }
     }
 }
