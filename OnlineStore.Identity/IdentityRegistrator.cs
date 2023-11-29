2a0776a69090303a6dbd54e97acbbef547f160f9
Swift <swiftx37@gmail.com>
2023-11-29 14:42:20 +0100

    Identity layer registrator.

---
+++ OnlineStore.Identity/IdentityRegistrator.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OnlineStore.Application.Interfaces.Identity;
+using OnlineStore.Identity.Context;
+using OnlineStore.Identity.Models;
+using OnlineStore.Identity.OptionsSetup;
+using OnlineStore.Identity.Services;
+
+namespace OnlineStore.Identity
+{
+    public static class IdentityRegistrator
+    {
+        public static IServiceCollection AddIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationIdentityDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("IdentityDbConnection")));
+            
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationIdentityDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddTransient<IAuthService, AuthService>();
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer();
+
+            services.ConfigureOptions<JwtOptionsSetup>();
+            services.ConfigureOptions<JwtBearerOptionsSetup>();
+
+            return services;
+        }
+    }
+}
