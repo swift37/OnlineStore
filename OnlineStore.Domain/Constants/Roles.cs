06ab5ae1e1bc32c5b329bb9f2f1e9ac871cd86b2
Swift <swiftx37@gmail.com>
2023-12-01 17:13:30 +0100

    Adding constants for defining roles.

---
+++ OnlineStore.Domain/Constants/Roles.cs
@@ -0,0 +1,10 @@
+﻿namespace OnlineStore.Domain.Constants
+{
+    public static class Roles
+    {
+        public const string Administrator = "Administrator";
+        public const string Manager = $"{Administrator},Manager";
+        public const string Employee = $"{Manager},Employee";
+        public const string User = $"{Employee},User";
+    }
+}
