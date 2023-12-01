f89927a746c9afe534bfa170837d06f61048cc29
Swift <swiftx37@gmail.com>
2023-12-01 20:41:47 +0100

    Creating the subscribers repository interface.

---
+++ OnlineStore.Application/Interfaces/Repositories/ISubscribersRepository.cs
@@ -0,0 +1,11 @@
+﻿using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Application.Interfaces.Repositories
+{
+    public interface ISubscribersRepository : IRepository<Subscriber>
+    {
+        Task<Subscriber> GetAsync(
+            string? email,
+            CancellationToken cancellation = default);
+    }
+}
