8fedf38fd9b9d9b9f9bb01e7c80b743b8d45f876
Swift <swiftx37@gmail.com>
2023-12-01 19:00:37 +0100

    Changing the access level for some methods in the reviews controller.

--- OnlineStore.WebAPI/Controllers/ReviewsController.cs
+++ OnlineStore.WebAPI/Controllers/ReviewsController.cs
@@ -28,7 +28,7 @@ namespace OnlineStore.WebAPI.Controllers
         /// <response code="401">If the user is unauthorized</response>
         /// <response code="403">If the user does not have the required access level</response>
         [HttpGet]
-        [Authorize(Roles = Roles.Administrator)]
+        [Authorize(Roles = Roles.Employee)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
@@ -48,7 +48,7 @@ namespace OnlineStore.WebAPI.Controllers
         /// <response code="401">If the user is unauthorized</response>
         /// <response code="403">If the user does not have the required access level</response>
         [HttpGet("exists/{id:int}")]
-        [Authorize(Roles = Roles.Administrator)]
+        [Authorize(Roles = Roles.Employee)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
@@ -68,7 +68,7 @@ namespace OnlineStore.WebAPI.Controllers
         /// <response code="401">If the user is unauthorized</response>
         /// <response code="403">If the user does not have the required access level</response>
         [HttpGet("{id:int}")]
-        [Authorize(Roles = Roles.Administrator)]
+        [Authorize(Roles = Roles.Employee)]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         [ProducesResponseType(StatusCodes.Status403Forbidden)]
