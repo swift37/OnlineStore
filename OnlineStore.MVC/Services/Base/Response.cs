152f1c13ec40944b0b12791099aca07019d88a6a
Swift <swiftx37@gmail.com>
2023-12-02 20:34:24 +0100

    Creating the response model.

---
+++ OnlineStore.MVC/Services/Base/Response.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineStore.MVC.Services.Base
+{
+    public class Response<T>
+    {
+        public string Message { get; set; } = string.Empty;
+
+        public string ValidationErrors { get; set; } = string.Empty;
+
+        public bool Success { get; set; }
+
+        public T Data { get; set; } = default!;
+    }
+}
