97ef5c7d9868db09f73eeb8f2e85c201edded9f2
Swift <swiftx37@gmail.com>
2023-12-02 20:33:12 +0100

    Generating the API client.

---
+++ OnlineStore.MVC/Services/ApiClient/Client.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineStore.MVC.Services.ApiClient
+{
+    public partial class Client : IClient
+    {
+        public HttpClient HttpClient => _httpClient;
+    }
+}
---
+++ OnlineStore.MVC/Services/ApiClient/IClient.cs
@@ -0,0 +1,7 @@
+﻿namespace OnlineStore.MVC.Services.ApiClient
+{
+    public partial interface IClient
+    {
+        public HttpClient HttpClient { get; }
+    }
+}
