8a281a347ab0605384ea2ee14dae66a0a499d055
Swift <swiftx37@gmail.com>
2023-12-04 20:27:31 +0100

    Authentication service interface.

---
+++ OnlineStore.MVC/Services/Interfaces/IAuthService.cs
@@ -0,0 +1,16 @@
+﻿using OnlineStore.MVC.Models;
+using OnlineStore.WebAPI.Models;
+
+namespace OnlineStore.MVC.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        Task Register(RegisterViewModel registerViewModel);
+
+        Task<IdentityResponse> Login(LoginViewModel loginViewModel);
+
+        Task<IdentityResponse> Refresh(string refreshToken);
+
+        Task Logout();
+    }
+}
