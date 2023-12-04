a50f76186e106e38d7ecd7fd87d7abd5b451acec
Swift <swiftx37@gmail.com>
2023-12-04 20:20:52 +0100

    View models for authentication.

---
+++ OnlineStore.MVC/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.WebAPI.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string UsernameOrEmail { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public string? ReturnUrl { get; set; }
+    }
+}
