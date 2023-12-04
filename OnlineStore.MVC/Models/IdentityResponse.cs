ed000f488ea1a0d5ad59e85a671001cb2fa22987
Swift <swiftx37@gmail.com>
2023-12-04 20:21:07 +0100

    Identity response model.

---
+++ OnlineStore.MVC/Models/IdentityResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineStore.MVC.Models
+{
+    public class IdentityResponse
+    {
+        public required string AccessToken { get; set; }
+
+        public required string RefreshToken { get; set; }
+    }
+}
