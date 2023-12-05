033414a694ce1ead000c0b85dadc45f9e36cf801
Swift <swiftx37@gmail.com>
2023-12-05 17:19:11 +0100

    Creating the products page view model.

---
+++ OnlineStore.MVC/Models/Product/ProductsPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using OnlineStore.MVC.Models.Category;
+
+namespace OnlineStore.MVC.Models.Product
+{
+    public class ProductsPageViewModel
+    {
+        public ICollection<ProductViewModel> Products { get; set; } = new HashSet<ProductViewModel>();
+
+        public CategoryViewModel? Category { get; set; }
+
+        public int CurrentPage { get; set; }
+
+        public int TotalPages { get; set; }
+
+        public int ItemsPerPage { get; set; }
+    }
+}
