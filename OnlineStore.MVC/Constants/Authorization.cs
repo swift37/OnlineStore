b969ad763061cf845610357167b5115ff0ecb56f
Swift <swiftx37@gmail.com>
2023-12-06 17:23:16 +0100

    Create authorization constants.

---
+++ OnlineStore.MVC/Constants/Authorization.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineStore.MVC.Constants
+{
+    public class Authorization
+    {
+        public const string XAccessToken = "X-Access-Token";
+
+        public const string Key = "Authorization";
+    }
+}
