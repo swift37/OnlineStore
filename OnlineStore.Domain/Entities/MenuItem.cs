d4555082c3b534f9136d18b03e51ffa8f8cb7852
Swift <swiftx37@gmail.com>
2023-12-10 19:29:36 +0100

    Creating MenuItem and NestedMenuItem models.

--- OnlineStore.Domain/Entities/MenuItem.cs
+++ OnlineStore.Domain/Entities/MenuItem.cs
@@ -2,14 +2,27 @@
 
 namespace OnlineStore.Domain.Entities
 {
-    public class MenuItem : Entity
+    public class MenuItem : NamedEntity
     {
         public int CategoryId { get; set; }
 
         public Category? Category { get; set; }
 
-        public ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+        public bool IsMegaMenu { get; set; }
+
+        public ICollection<NestedMenuItem> NestedItems { get; set; } = new HashSet<NestedMenuItem>();
 
         public string? Image { get; set; }
+    }
+
+    public class NestedMenuItem : NamedEntity
+    {
+        public int ParentId { get; set; }
+
+        public MenuItem? Parent { get; set; }
+
+        public ICollection<Category> Categories { get; set; } = new HashSet<Category>();
+
+        public bool HasTwoColumns { get; set; }
     }
 }
