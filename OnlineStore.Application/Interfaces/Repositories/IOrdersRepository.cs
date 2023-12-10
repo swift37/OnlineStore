bbd9cf4cda37a5f1303ff0a5127cb79b4db50068
Swift <swiftx37@gmail.com>
2023-12-10 23:49:39 +0100

    Signature for Get (order by id) method in the orders repository interface.

--- OnlineStore.Application/Interfaces/Repositories/IOrdersRepository.cs
+++ OnlineStore.Application/Interfaces/Repositories/IOrdersRepository.cs
@@ -4,6 +4,10 @@ namespace OnlineStore.Application.Interfaces.Repositories
 {
     public interface IOrdersRepository : IRepository<Order>
     {
+        Task<Order> GetAsync(
+            string number,
+            CancellationToken cancellation = default);
+
         Task<IEnumerable<Order>> GetUserOrdersAsync(
             Guid userId, 
             CancellationToken cancellation = default);
