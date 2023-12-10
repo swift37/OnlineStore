1ef503ae89c008ad4a23d21ef56d4b52e7ceeb20
Swift <swiftx37@gmail.com>
2023-12-10 23:50:36 +0100

    Implimentation of Get (order by id) method in the orders repository.

--- OnlineStore.Persistence/Repositories/OrdersRepository.cs
+++ OnlineStore.Persistence/Repositories/OrdersRepository.cs
@@ -3,12 +3,30 @@ using OnlineStore.Application.Exeptions;
 using OnlineStore.Application.Interfaces;
 using OnlineStore.Application.Interfaces.Repositories;
 using OnlineStore.Domain;
+using OnlineStore.Domain.Entities;
 
 namespace OnlineStore.DAL.Repositories
 {
     public class OrdersRepository : Repository<Order>, IOrdersRepository
     {
         public OrdersRepository(IApplicationDbContext context) : base(context) { }
+
+        public async Task<Order> GetAsync(string? number, CancellationToken cancellation = default)
+        {
+            if (number is null) throw new ArgumentNullException(nameof(number));
+
+            switch (Entities)
+            {
+                case DbSet<Order> dbSet:
+                    return await dbSet.FindAsync(new object[] { number }, cancellation).ConfigureAwait(false)
+                        ?? throw new NotFoundException(nameof(Order), number);
+                case { } entities:
+                    return await entities.FirstOrDefaultAsync(o => o.Number == number, cancellation).ConfigureAwait(false)
+                        ?? throw new NotFoundException(nameof(Order), number);
+                default:
+                    throw new InvalidOperationException("Data source defenition failed.");
+            }
+        }
 
         public async Task<IEnumerable<Order>> GetUserOrdersAsync(
             Guid userId, 
