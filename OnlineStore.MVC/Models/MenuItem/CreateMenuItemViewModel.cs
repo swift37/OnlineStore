cf7ec694df6b4704a5e1da4cf2cca37a5bfcc220
Swift <swiftx37@gmail.com>
2023-12-11 18:31:45 +0100

    Creating view models to represent the MenuItem model.

---
+++ OnlineStore.MVC/Models/MenuItem/CreateMenuItemViewModel.cs
@@ -0,0 +1,28 @@
+﻿using OnlineStore.MVC.Models.Category;
+
+namespace OnlineStore.MVC.Models.MenuItem
+{
+    public class CreateMenuItemViewModel
+    {
+        public string? NavigationLabel { get; set; }
+
+        public int CategoryId { get; set; }
+
+        public bool IsMegaMenu { get; set; }
+
+        public ICollection<CreateNestedMenuItemViewModel> NestedItems { get; set; } = new HashSet<CreateNestedMenuItemViewModel>();
+
+        public string? Image { get; set; }
+    }
+
+    public class CreateNestedMenuItemViewModel
+    {
+        public string? NavigationLabel { get; set; }
+
+        public int ParentId { get; set; }
+
+        public ICollection<CategoryViewModel> Categories { get; set; } = new HashSet<CategoryViewModel>();
+
+        public bool HasTwoColumns { get; set; }
+    }
+}
---
+++ OnlineStore.MVC/Models/MenuItem/MenuItemViewModel.cs
@@ -0,0 +1,36 @@
+﻿using OnlineStore.MVC.Models.Category;
+
+namespace OnlineStore.MVC.Models.MenuItem
+{
+    public class MenuItemViewModel
+    {
+        public int Id { get; set; }
+
+        public string? NavigationLabel {  get; set; } 
+
+        public int CategoryId { get; set; }
+
+        public CategoryViewModel? Category { get; set; }
+
+        public bool IsMegaMenu { get; set; }
+
+        public ICollection<NestedMenuItemViewModel> NestedItems { get; set; } = new HashSet<NestedMenuItemViewModel>();
+
+        public string? Image { get; set; }
+    }
+
+    public class NestedMenuItemViewModel
+    {
+        public int Id { get; set; }
+
+        public string? NavigationLabel { get; set; }
+
+        public int ParentId { get; set; }
+
+        public MenuItemViewModel? Parent { get; set; }
+
+        public ICollection<CategoryViewModel> Categories { get; set; } = new HashSet<CategoryViewModel>();
+
+        public bool HasTwoColumns { get; set; }
+    }
+}
