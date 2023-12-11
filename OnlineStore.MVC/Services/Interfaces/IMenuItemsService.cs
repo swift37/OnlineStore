ba65fb4867e616483fe7e07b2f862033dd0072da
Swift <swiftx37@gmail.com>
2023-12-11 19:30:35 +0100

    Creating the MenuItemService interface.

---
+++ OnlineStore.MVC/Services/Interfaces/IMenuItemsService.cs
@@ -0,0 +1,20 @@
+﻿using OnlineStore.MVC.Models.MenuItem;
+using OnlineStore.MVC.Services.Base;
+
+namespace OnlineStore.MVC.Services.Interfaces
+{
+    public interface IMenuItemsService
+    {
+        Task<Response<IEnumerable<MenuItemViewModel>>> GetAll();
+
+        Task<Response<bool>> Exist(int id);
+
+        Task<Response<MenuItemViewModel>> Get(int id);
+
+        Task<Response<int>> Create(CreateMenuItemViewModel createMenuItemViewModel);
+
+        Task<Response> Update(MenuItemViewModel menuItemViewModel);
+
+        Task<Response> Delete(int id);
+    }
+}
