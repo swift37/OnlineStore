c03ef6a12153380bf84226c31cd1387dc89326ab
Swift <swiftx37@gmail.com>
2023-12-14 23:47:24 +0100

    Adding the CartService interface.

---
+++ OnlineStore.MVC/Services/Interfaces/ICartService.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineStore.MVC.Services.Interfaces
+{
+    public interface ICartService
+    {
+        bool Add(int productId, int quantity = 1);
+
+        bool Decrement(int productId);
+
+        bool Remove(int productId);
+
+        void Clear();
+    }
+}
