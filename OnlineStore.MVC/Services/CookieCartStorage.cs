0bdcbaf0eed2f7666d2533cd229b285dfc77c429
Swift <swiftx37@gmail.com>
2023-12-14 23:47:53 +0100

    Implementation of the CartStorage.

---
+++ OnlineStore.MVC/Services/CookieCartStorage.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using OnlineStore.MVC.Models.Cart;
+using OnlineStore.MVC.Services.Interfaces;
+
+namespace OnlineStore.MVC.Services
+{
+    public class CookieCartStorage : ICartStorage
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly string _cartName;
+
+        private HttpRequest Request => _httpContextAccessor.HttpContext!.Request!;
+
+        private HttpResponse Response => _httpContextAccessor.HttpContext!.Response!;
+
+        public CartViewModel? Cart
+        {
+            get
+            {
+                var cookies = Response.Cookies;
+                var cartCookies = Request.Cookies[_cartName];
+                if (cartCookies is null)
+                {
+                    var cart = new CartViewModel();
+                    cookies.Append(_cartName, JsonConvert.SerializeObject(cart));
+                    return cart;
+                }
+
+                ReplaceCookies(cookies, cartCookies);
+                return JsonConvert.DeserializeObject<CartViewModel>(cartCookies);
+            }
+
+            set => ReplaceCookies(Response.Cookies, JsonConvert.SerializeObject(value));
+        }
+
+        public CookieCartStorage(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+
+            var user = httpContextAccessor.HttpContext!.User;
+            var username = (user.Identity?.IsAuthenticated ?? false) ? $"-{user.Identity.Name}" : null;
+            _cartName = Constants.Cart.CookieCartName + username;
+        }
+
+        private void ReplaceCookies(IResponseCookies cookies, string cookiesString)
+        {
+            cookies.Delete(_cartName);
+            cookies.Append(_cartName, cookiesString);
+        }
+    }
+}
