7e40455d562d0aba4fb9a387a0f05a7ab1248a75
Swift <swiftx37@gmail.com>
2023-12-14 23:51:02 +0100

    Creating the MiniCartViewComponent.

---
+++ OnlineStore.MVC/ViewComponents/MiniCartViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.MVC.Services.Interfaces;
+
+namespace OnlineStore.MVC.ViewComponents
+{
+    public class MiniCartViewComponent : ViewComponent
+    {
+        private readonly ICartStorage _cartStorage;
+
+        public MiniCartViewComponent(ICartStorage cartStorage) => _cartStorage = cartStorage;
+
+        public Task<IViewComponentResult> InvokeAsync() => 
+            Task.FromResult<IViewComponentResult>(View(_cartStorage.Cart)); 
+    }
+}
