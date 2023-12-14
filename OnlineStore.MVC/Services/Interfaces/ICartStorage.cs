8d6f0dc5665a51afd32dc3771409372ec861e4fd
Swift <swiftx37@gmail.com>
2023-12-14 23:47:09 +0100

    Adding the CartStorage interface.

---
+++ OnlineStore.MVC/Services/Interfaces/ICartStorage.cs
@@ -0,0 +1,9 @@
+﻿using OnlineStore.MVC.Models.Cart;
+
+namespace OnlineStore.MVC.Services.Interfaces
+{
+    public interface ICartStorage
+    {
+        CartViewModel? Cart { get; set; }
+    }
+}
