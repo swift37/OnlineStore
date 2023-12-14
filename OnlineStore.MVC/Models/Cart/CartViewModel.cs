75430761d138c8908d94c6f04bfe4f6eaf4f9cc5
Swift <swiftx37@gmail.com>
2023-12-14 23:46:11 +0100

    Creating the CartViewModel.

---
+++ OnlineStore.MVC/Models/Cart/CartViewModel.cs
@@ -0,0 +1,20 @@
+﻿using OnlineStore.MVC.Models.Product;
+
+namespace OnlineStore.MVC.Models.Cart
+{
+    public class CartViewModel
+    {
+        public ICollection<CartItemViewModel> Items { get; set; } = new HashSet<CartItemViewModel>();
+
+        public int ItemsQuantity => Items.Sum(i => i.Quantity);
+    }
+
+    public class CartItemViewModel
+    {
+        public int ProductId { get; set; }
+
+        public ProductViewModel? Product { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
