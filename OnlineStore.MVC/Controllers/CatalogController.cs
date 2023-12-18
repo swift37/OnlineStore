65f2de25fbf1051c45fbccd1181e8f6ec96a2412
Swift <swiftx37@gmail.com>
2023-12-18 18:46:10 +0100

    Displaying the products by categories.

--- OnlineStore.MVC/Controllers/CatalogController.cs
+++ OnlineStore.MVC/Controllers/CatalogController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.MVC.Models.Enums;
+using OnlineStore.MVC.Services.Interfaces;
 
 namespace OnlineStore.MVC.Controllers
 {
     public class CatalogController : Controller
     {
-        public IActionResult Index()
+        private readonly IProductsService _productsService;
+
+        public CatalogController(IProductsService productsService) => 
+            _productsService = productsService;
+
+        [HttpGet]
+        public async Task<IActionResult> Index(
+            int categoryId, 
+            int page = 1, 
+            int itemsPerPage = 15, 
+            SortParameters sortBy = SortParameters.Default)
         {
-            return View();
+            var response = await _productsService
+                .GetProductsByCategory(categoryId, page, itemsPerPage, sortBy);
+
+            if (response.Success) return View(response.Data);
+
+            return StatusCode(response.Status);
         }
     }
 }
