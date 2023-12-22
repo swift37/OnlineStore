034f9bb3e8287c277cca88f5d995ca057649630b
Swift <swiftx37@gmail.com>
2023-12-22 23:21:00 +0100

    Dictionary extension for cast IDictionary<string, string[]> to IEnumerable<ValidationFailure>.

---
+++ OnlineStore.MVC/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using OnlineStore.MVC.Services.Base;
+
+namespace OnlineStore.MVC.Extensions
+{
+    public static class DictionaryExtensions
+    {
+        public static IEnumerable<ValidationFailure> ToValidationFailures(this IDictionary<string, string[]> dictionary)
+        {
+            var validationFailures = new List<ValidationFailure>();
+
+            foreach (var pair in dictionary)
+                foreach (var failure in pair.Value)
+                    validationFailures.Add(new ValidationFailure(pair.Key, failure));
+
+            return validationFailures;
+        }
+    }
+}
