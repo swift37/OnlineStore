4e5b076eb14d72fe5730387b28ebf18dc976a312
Swift <swiftx37@gmail.com>
2023-12-22 23:15:00 +0100

    Creating the BadResponse model for deserialization validation failures from server.

---
+++ OnlineStore.MVC/Services/Base/BadResponse.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json.Serialization;
+
+namespace OnlineStore.MVC.Services.Base
+{
+    public class BadResponse
+    {
+        [JsonPropertyName("title")]
+        public string? Title { get; set; }
+
+        [JsonPropertyName("status")]
+        public int Status { get; set; }
+
+        [JsonPropertyName("errors")]
+        public IDictionary<string, string[]> Errors { get; set; } = new Dictionary<string, string[]>();
+    }
+}
