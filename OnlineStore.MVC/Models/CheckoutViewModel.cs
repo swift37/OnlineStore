fec47d0822ae13824097e9540806eda8fd5bc766
Swift <swiftx37@gmail.com>
2023-12-22 20:34:06 +0100

    Creating the CheckoutViewModel.

---
+++ OnlineStore.MVC/Models/CheckoutViewModel.cs
@@ -0,0 +1,12 @@
+﻿using OnlineStore.MVC.Models.Cart;
+using OnlineStore.MVC.Models.Order;
+
+namespace OnlineStore.MVC.Models
+{
+    public class CheckoutViewModel
+    {
+        public CartViewModel Cart { get; set; } = new();
+
+        public CreateOrderViewModel Order { get; set; } = new();
+    }
+}
