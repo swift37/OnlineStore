9f2d07f7530e94de9e4955336cb3b4ac5d6512b0
Swift <swiftx37@gmail.com>
2023-12-24 15:51:43 +0100

    Categories repository interface.

---
+++ OnlineStore.Application/Interfaces/Repositories/ICategoriesRepository.cs
@@ -0,0 +1,9 @@
+﻿using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Application.Interfaces.Repositories
+{
+    public interface ICategoriesRepository : IRepository<Category>
+    {
+        Task<IEnumerable<Category>> GetMainCategoriesAsync(CancellationToken cancellation = default);
+    }
+}
