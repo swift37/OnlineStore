cac7dab650a53f1952f1b3f658ecafe743111d5d
Swift <swiftx37@gmail.com>
2023-12-25 23:52:00 +0100

    Creating the AccountMenuViewComponent.

---
+++ OnlineStore.MVC/ViewComponents/AccountMenuViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineStore.MVC.ViewComponents
+{
+    public class AccountMenuViewComponent : ViewComponent
+    {
+        public Task<IViewComponentResult> InvokeAsync() =>
+            Task.FromResult<IViewComponentResult>(View());
+    }
+}
