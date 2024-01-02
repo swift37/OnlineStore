3c698272b12d33bab3db7e44ad1a6cae843fcd2a
Swift <swiftx37@gmail.com>
2024-01-02 21:31:12 +0100

    Created the ProductsFilteringOptions model.

---
+++ OnlineStore.Domain/ProductsFilteringOptions.cs
@@ -0,0 +1,22 @@
+﻿using OnlineStore.Domain.Entities;
+using OnlineStore.Domain.Enums;
+
+namespace OnlineStore.Domain
+{
+    public class ProductsFilteringOptions
+    {
+        public int CategoryId { get; set; }
+
+        public int MinPrice { get; set; }
+
+        public int MaxPrice { get; set; }
+
+        public ICollection<Specification> Specifications { get; set; } = new HashSet<Specification>();
+
+        public int PageNumber {  get; set; }
+
+        public int ItemsPerPage { get; set; }
+
+        public SortParameter SortBy { get; set; }
+    }
+}
