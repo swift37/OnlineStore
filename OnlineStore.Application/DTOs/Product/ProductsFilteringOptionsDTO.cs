cdabdf833f9c7577a97514142cfe26b4035db0e2
Swift <swiftx37@gmail.com>
2024-01-02 21:52:26 +0100

    Created ProductsFilteringOptionsDTO and validator for it.

---
+++ OnlineStore.Application/DTOs/Product/ProductsFilteringOptionsDTO.cs
@@ -0,0 +1,21 @@
+﻿using OnlineStore.Domain.Enums;
+
+namespace OnlineStore.Application.DTOs.Product
+{
+    public class ProductsFilteringOptionsDTO
+    {
+        public int CategoryId { get; set; }
+
+        public int MinPrice { get; set; }
+
+        public int MaxPrice { get; set; }
+
+        public ICollection<SpecificationDTO> Specifications { get; set; } = new HashSet<SpecificationDTO>();
+
+        public int PageNumber { get; set; }
+
+        public int ItemsPerPage { get; set; }
+
+        public SortParameter SortBy { get; set; }
+    }
+}
