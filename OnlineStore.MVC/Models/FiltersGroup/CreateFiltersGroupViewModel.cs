30c54407b8a3a43f2f4d0c14537a478b60b36b24
Swift <swiftx37@gmail.com>
2024-01-03 23:18:52 +0100

    Added the FiltersGroup view models to the MVC project.

---
+++ OnlineStore.MVC/Models/FiltersGroup/CreateFiltersGroupViewModel.cs
@@ -0,0 +1,11 @@
+﻿using OnlineStore.MVC.Services.ApiClient;
+
+namespace OnlineStore.MVC.Models.FiltersGroup
+{
+    public class CreateFiltersGroupViewModel
+    {
+        public int CategoryId { get; set; }
+
+        public ICollection<SpecificationDTO> Specifications { get; set; } = new HashSet<SpecificationDTO>();
+    }
+}
---
+++ OnlineStore.MVC/Models/FiltersGroup/FiltersGroupViewModel.cs
@@ -0,0 +1,16 @@
+﻿using OnlineStore.MVC.Models.Category;
+using OnlineStore.MVC.Services.ApiClient;
+
+namespace OnlineStore.MVC.Models.FiltersGroup
+{
+    public class FiltersGroupViewModel
+    {
+        public int Id { get; set; } 
+
+        public int CategoryId { get; set; }
+
+        public CategoryViewModel? Category { get; set; }
+
+        public IDictionary<string, FilterDTO> Filters { get; set; } = new Dictionary<string, FilterDTO>(); 
+    }
+}
