fe2caf00478bde3d8d8db512ed1647471090caf3
Swift <swiftx37@gmail.com>
2024-01-03 23:01:19 +0100

    Validators for the FiltersGroup DTOs.

---
+++ OnlineStore.Application/DTOs/FiltersGroup/Validation/CreateFiltersGroupDTOValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace OnlineStore.Application.DTOs.FiltersGroup.Validation
+{
+    public class CreateFiltersGroupDTOValidator : AbstractValidator<CreateFiltersGroupDTO>
+    {
+        public CreateFiltersGroupDTOValidator()
+        {
+            RuleFor(filtersGroup => filtersGroup.CategoryId)
+                .GreaterThan(0);
+        }
+    }
+}
---
+++ OnlineStore.Application/DTOs/FiltersGroup/Validation/FiltersGroupDTOValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+
+namespace OnlineStore.Application.DTOs.FiltersGroup.Validation
+{
+    public class FiltersGroupDTOValidator : AbstractValidator<FiltersGroupDTO>
+    {
+        public FiltersGroupDTOValidator()
+        {
+            RuleFor(filtersGroup => filtersGroup.Id)
+                .GreaterThan(0);
+
+            RuleFor(filtersGroup => filtersGroup.CategoryId)
+                .GreaterThan(0);
+        }
+    }
+}
