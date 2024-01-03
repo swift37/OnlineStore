9a16bcb4064ed215f69b73de3fd91cca81b8ae8b
Swift <swiftx37@gmail.com>
2024-01-03 23:13:51 +0100

    FilterGroups repository interface.

---
+++ OnlineStore.Application/Interfaces/Repositories/IFilterGroupsRepository.cs
@@ -0,0 +1,11 @@
+﻿using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Application.Interfaces.Repositories
+{
+    public interface IFilterGroupsRepository : IRepository<FiltersGroup>
+    {
+        Task<FiltersGroup> GetCategoryFiltersGroupAsync(
+            int categoryId, 
+            CancellationToken cancellation = default);
+    }
+}
