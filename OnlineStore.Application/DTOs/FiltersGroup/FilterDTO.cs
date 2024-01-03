42362aa37cb2c9d4d7795e39394e474de27e9f96
Swift <swiftx37@gmail.com>
2024-01-03 23:00:24 +0100

    Created the FilterDTO.

---
+++ OnlineStore.Application/DTOs/FiltersGroup/FilterDTO.cs
@@ -0,0 +1,11 @@
+﻿using OnlineStore.Application.DTOs.Product;
+
+namespace OnlineStore.Application.DTOs.FiltersGroup
+{
+    public class FilterDTO
+    {
+        public string? Value { get; set; }
+
+        public int ProductsCount { get; set; }
+    }
+}
