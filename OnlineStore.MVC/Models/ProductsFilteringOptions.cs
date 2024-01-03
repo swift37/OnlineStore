6c870b08034259eaf087f704f8c3732af76710e5
Swift <swiftx37@gmail.com>
2024-01-03 23:18:22 +0100

    Added the products filtering options model to the MVC project.

---
+++ OnlineStore.MVC/Models/ProductsFilteringOptions.cs
@@ -0,0 +1,21 @@
+﻿using OnlineStore.MVC.Services.ApiClient;
+
+namespace OnlineStore.MVC.Models
+{
+    public class ProductsFilteringOptions
+    {
+        public int CategoryId { get; set; }
+
+        public int MinPrice { get; set; }
+
+        public int MaxPrice { get; set; }
+
+        public ICollection<SpecificationDTO> Specifications { get; set; } = new HashSet<SpecificationDTO>();
+
+        public int PageNumber { get; set; }
+
+        public int ItemsPerPage { get; set; }
+
+        public SortParameter SortBy { get; set; }
+    }
+}
