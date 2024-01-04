7573ed178281434efec6893d3c6639b172d21afb
Swift <swiftx37@gmail.com>
2024-01-04 22:59:27 +0100

    Entity type configuration for the SpecificationType domain model.

---
+++ OnlineStore.Persistence/EntityTypeConfigurations/SpecificationTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineStore.DAL.EntityTypeConfigurations.Base;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.DAL.EntityTypeConfigurations
+{
+    public class SpecificationTypeConfiguration : BaseConfiguration<SpecificationType>
+    {
+        public override void Configure(EntityTypeBuilder<SpecificationType> builder)
+        {
+            base.Configure(builder);
+            builder.Property(specificationType => specificationType.Name).HasMaxLength(32);
+            builder.Property(specificationType => specificationType.DisplayName).HasMaxLength(32);
+            builder
+                .HasMany(specificationType => specificationType.Values)
+                .WithOne(specification => specification.SpecificationType)
+                .HasForeignKey(specification => specification.SpecificationTypeId)
+                .OnDelete(DeleteBehavior.Cascade)
+                .IsRequired();
+            builder.Navigation(specificationType => specificationType.Values).AutoInclude();
+        }
+    }
+}
