b69bf0033a9d18ab7f3048eb85e8ed2cf78c4fea
Swift <swiftx37@gmail.com>
2024-01-04 22:56:50 +0100

    Validation for the SpecificationType DTOs.

---
+++ OnlineStore.Application/DTOs/SpecificationType/Validation/SpecificationTypeDTOValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace OnlineStore.Application.DTOs.SpecificationType.Validation
+{
+    public class SpecificationTypeDTOValidator : AbstractValidator<SpecificationTypeDTO>
+    {
+        public SpecificationTypeDTOValidator()
+        {
+            RuleFor(s => s.Id)
+                .GreaterThan(s => s.Id);
+
+            RuleFor(s => s.Name)
+                .NotEmpty()
+                .MaximumLength(32);
+
+            RuleFor(s => s.DisplayName)
+                .NotEmpty()
+                .MaximumLength(32);
+        }
+    }
+}
---
+++ OnlineStore.Application/DTOs/SpecificationType/Validation/UpdateSpecificationTypeDTOValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+
+namespace OnlineStore.Application.DTOs.SpecificationType.Validation
+{
+    public class UpdateSpecificationTypeDTOValidator : AbstractValidator<UpdateSpecificationTypeDTO>
+    {
+        public UpdateSpecificationTypeDTOValidator()
+        {
+            RuleFor(s => s.Id)
+                .GreaterThan(s => s.Id);
+
+            RuleFor(s => s.Name)
+                .NotEmpty()
+                .MaximumLength(32);
+
+            RuleFor(s => s.DisplayName)
+                .NotEmpty()
+                .MaximumLength(32);
+        }
+    }
+}
