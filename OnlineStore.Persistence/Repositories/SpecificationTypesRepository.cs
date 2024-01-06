10d44729b3039e0fb2ba78ca2fad866b13bf8ba3
Swift <swiftx37@gmail.com>
2024-01-06 23:05:00 +0100

    Added the specifications and specification types repositories.

---
+++ OnlineStore.Persistence/Repositories/SpecificationTypesRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Application.Interfaces;
+using OnlineStore.DAL.Repositories;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Persistence.Repositories
+{
+    public class SpecificationTypesRepository : Repository<SpecificationType>
+    {
+        protected override IQueryable<SpecificationType> Entities => base.Entities
+            .Include(specificationType => specificationType.Values);
+
+        public SpecificationTypesRepository(IApplicationDbContext context) : base(context) { }
+    }
+}
---
+++ OnlineStore.Persistence/Repositories/SpecificationsRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Application.Interfaces;
+using OnlineStore.DAL.Repositories;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Persistence.Repositories
+{
+    public class SpecificationsRepository : Repository<Specification>
+    {
+        protected override IQueryable<Specification> Entities => base.Entities
+            .Include(specification => specification.SpecificationType);
+
+        public SpecificationsRepository(IApplicationDbContext context) : base(context) { }
+    }
+}
