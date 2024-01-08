18aff35be53dbfc6dc9a86a3b80369a1e191c64c
Swift <swiftx37@gmail.com>
2024-01-08 18:44:44 +0100

    SpecificationTypesService interface.

---
+++ OnlineStore.MVC/Services/Interfaces/ISpecificationTypesService.cs
@@ -0,0 +1,20 @@
+﻿using OnlineStore.MVC.Models.SpecificationType;
+using OnlineStore.MVC.Services.Base;
+
+namespace OnlineStore.MVC.Services.Interfaces
+{
+    public interface ISpecificationTypesService
+    {
+        Task<Response<IEnumerable<SpecificationTypeViewModel>>> GetAll();
+
+        Task<Response<bool>> Exist(int id);
+
+        Task<Response<SpecificationTypeViewModel>> Get(int id);
+
+        Task<Response<int>> Create(CreateSpecificationTypeViewModel createSpecificationTypeViewModel);
+
+        Task<Response> Update(SpecificationTypeViewModel specificationTypeViewModel);
+
+        Task<Response> Delete(int id);
+    }
+}
