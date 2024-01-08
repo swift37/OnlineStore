4bfd6d192eb82a1ebac7ab5a7eff8bf082c45d25
Swift <swiftx37@gmail.com>
2024-01-08 18:46:41 +0100

    SpecificationType view models created.

---
+++ OnlineStore.MVC/Models/SpecificationType/SpecificationTypeViewModel.cs
@@ -0,0 +1,17 @@
+﻿using OnlineStore.MVC.Models.Specification;
+
+namespace OnlineStore.MVC.Models.SpecificationType
+{
+    public class SpecificationTypeViewModel
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? DisplayName { get; set; }
+
+        public bool IsMain { get; set; }
+
+        public ICollection<SpecificationViewModel> Values { get; set; } = new HashSet<SpecificationViewModel>();
+    }
+}
