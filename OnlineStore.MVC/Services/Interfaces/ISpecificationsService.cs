c2a0ab8be170e41f4e32a3e47ca98a238a52afdb
Swift <swiftx37@gmail.com>
2024-01-08 18:44:33 +0100

    SpecificationsService interface.

---
+++ OnlineStore.MVC/Services/Interfaces/ISpecificationsService.cs
@@ -0,0 +1,20 @@
+﻿using OnlineStore.MVC.Models.Specification;
+using OnlineStore.MVC.Services.Base;
+
+namespace OnlineStore.MVC.Services.Interfaces
+{
+    public interface ISpecificationsService
+    {
+        Task<Response<IEnumerable<SpecificationViewModel>>> GetAll();
+
+        Task<Response<bool>> Exist(int id);
+
+        Task<Response<SpecificationViewModel>> Get(int id);
+
+        Task<Response<int>> Create(CreateSpecificationViewModel createSpecificationViewModel);
+
+        Task<Response> Update(SpecificationViewModel specificationViewModel);
+
+        Task<Response> Delete(int id);
+    }
+}
