83edfa1c4578dc703e1336c88456c877530828ab
Swift <swiftx37@gmail.com>
2024-01-08 18:46:32 +0100

    Specification view models created.

---
+++ OnlineStore.MVC/Models/Specification/CreateSpecificationViewModel.cs
@@ -0,0 +1,13 @@
+﻿using OnlineStore.MVC.Models.Product;
+
+namespace OnlineStore.MVC.Models.Specification
+{
+    public class CreateSpecificationViewModel
+    {
+        public int SpecificationTypeId { get; set; }
+
+        public string? Value { get; set; }
+
+        public ICollection<ProductViewModel> Products { get; set; } = new HashSet<ProductViewModel>();
+    }
+}
---
+++ OnlineStore.MVC/Models/Specification/SpecificationViewModel.cs
@@ -0,0 +1,20 @@
+﻿using OnlineStore.MVC.Models.Product;
+using OnlineStore.MVC.Models.SpecificationType;
+
+namespace OnlineStore.MVC.Models.Specification
+{
+    public class SpecificationViewModel
+    {
+        public int Id { get; set; }
+
+        public int SpecificationTypeId { get; set; }
+
+        public SpecificationTypeViewModel? SpecificationType { get; set; }
+
+        public string? Value { get; set; }
+
+        public ICollection<ProductViewModel> Products { get; set; } = new HashSet<ProductViewModel>();
+
+        public int ProductsCount { get; set; }
+    }
+}
