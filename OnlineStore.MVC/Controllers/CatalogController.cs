f453220f78330b79eb8e7bcb6518295a229785b2
Swift <swiftx37@gmail.com>
2024-01-09 23:13:09 +0100

    Corrected catalog index GET method.

--- OnlineStore.MVC/Controllers/CatalogController.cs
+++ OnlineStore.MVC/Controllers/CatalogController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OnlineStore.MVC.Attributes;
+using OnlineStore.MVC.Models;
 using OnlineStore.MVC.Models.Enums;
 using OnlineStore.MVC.Models.Review;
 using OnlineStore.MVC.Services.Interfaces;
@@ -11,23 +12,56 @@ namespace OnlineStore.MVC.Controllers
     {
         private readonly IProductsService _productsService;
         private readonly IReviewsService _reviewsService;
+        private readonly IFilterGroupsService _filterGroupsService;
 
-        public CatalogController(IProductsService productsService, IReviewsService reviewsService) => 
-            (_productsService, _reviewsService) = (productsService, reviewsService);
+        public CatalogController(
+            IProductsService productsService, 
+            IReviewsService reviewsService,
+            IFilterGroupsService filterGroupsService) => 
+            (_productsService, _reviewsService, _filterGroupsService) = (productsService, reviewsService, filterGroupsService);
 
         [HttpGet]
         public async Task<IActionResult> Index(
             int categoryId, 
             int page = 1, 
-            int itemsPerPage = 15, 
-            SortParameters sortBy = SortParameters.Default)
+            int itemsPerPage = 15,
+            int minPrice = 0,
+            int maxPrice = int.MaxValue,
+            SortParameter sortBy = SortParameter.Default)
         {
-            var response = await _productsService
-                .GetProductsByCategory(categoryId, page, itemsPerPage, sortBy);
+            var options = new ProductsFilteringOptions
+            {
+                CategoryId = categoryId,
+                MinPrice = minPrice,
+                MaxPrice = maxPrice,
+                PageNumber = page,
+                ItemsPerPage = itemsPerPage,
+                SortBy = sortBy
+            };
+           
+            var specIds = HttpContext.Request.Query["filters"];
+            foreach (var stringId in specIds)
+            {
+                if (int.TryParse(stringId?.Split(';')[0][4..], out var specTypeId) &&
+                    int.TryParse(stringId?.Split(';')[1][3..], out var specId))
+                    if (!options.SpecificationIds.TryAdd(specTypeId, new List<int> { specId }))
+                        options.SpecificationIds[specTypeId].Add(specId);
+            }
 
-            if (response.Success) return View(response.Data);
+            var productsServiceResponse = await _productsService.GetFilteredProducts(options);
+            if (!productsServiceResponse.Success) return StatusCode(productsServiceResponse.Status);
 
-            return StatusCode(response.Status);
+            var filterGroupsServiceResponse = await _filterGroupsService.GetCategoryFiltersGroup(categoryId);
+            if (!filterGroupsServiceResponse.Success) return StatusCode(productsServiceResponse.Status);
+
+            var model = new CatalogViewModel
+            {
+                FiltersGroup = filterGroupsServiceResponse.Data,
+                AppliedFilterIds = options.SpecificationIds.Values.SelectMany(v => v).ToList(),
+                ProductsPage = productsServiceResponse.Data,
+            };
+
+            return View(model);
         }
 
         [HttpGet]
