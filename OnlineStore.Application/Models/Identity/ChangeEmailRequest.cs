3f906a13fb31328cc8e84e2ea34a4cc291344852
Swift <swiftx37@gmail.com>
2024-01-17 21:36:01 +0100

    Added the ChangeEmailRequest, ConfirmEmailChangingRequest and ConfirmEmailRequest models.

---
+++ OnlineStore.Application/Models/Identity/ChangeEmailRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.Application.Models.Identity
+{
+    public class ChangeEmailRequest
+    {
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string NewEmail { get; set; }
+    }
+}
