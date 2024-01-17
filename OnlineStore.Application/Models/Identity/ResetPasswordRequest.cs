986fb881c66ec073ec93516c85dd452beaf5517b
Swift <swiftx37@gmail.com>
2024-01-17 21:35:09 +0100

    Added the ResetPasswordRequest model.

---
+++ OnlineStore.Application/Models/Identity/ResetPasswordRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.Application.Models.Identity
+{
+    public class ResetPasswordRequest
+    {
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string Token { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword")]
+        public string NewPasswordConfirmation { get; set; }
+    }
+}
