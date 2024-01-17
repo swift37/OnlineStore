44609d0ebdbf102de58621a94aa7b99988983b16
Swift <swiftx37@gmail.com>
2024-01-17 21:34:57 +0100

    Added the ChangePasswordRequest model.

---
+++ OnlineStore.Application/Models/Identity/ChangePasswordRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.Application.Models.Identity
+{
+    public class ChangePasswordRequest
+    {
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword")]
+        public string NewPasswordConfirmation { get; set; }
+    }
+}
