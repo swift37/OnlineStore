1b49f5cec97397f61b44390543d24eac3314aa21
Swift <swiftx37@gmail.com>
2024-01-17 21:33:58 +0100

    Added the UpdateUserRequest model.

---
+++ OnlineStore.Application/Models/Identity/UpdateUserRequest.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.Application.Models.Identity
+{
+    public class UpdateUserRequest
+    {
+        [Required]
+        public Guid UserId { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
