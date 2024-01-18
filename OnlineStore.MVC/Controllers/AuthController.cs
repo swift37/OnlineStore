8dc8b2cc14914892579b53c24c81a84a2b6ea5dd
Swift <swiftx37@gmail.com>
2024-01-18 21:32:01 +0100

    Added actions for email confirmation and password reset to the AuthController in the MVC project.

--- OnlineStore.MVC/Controllers/AuthController.cs
+++ OnlineStore.MVC/Controllers/AuthController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using OnlineStore.MVC.Constants;
 using OnlineStore.MVC.Models;
+using OnlineStore.MVC.Services.ApiClient;
 using OnlineStore.MVC.Services.Interfaces;
 
 namespace OnlineStore.MVC.Controllers
@@ -108,6 +109,70 @@ namespace OnlineStore.MVC.Controllers
             }
 
             return RedirectToAction("Login");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ConfirmEmail(Guid userId, string token)
+        {
+            var response = await _authService.ConfirmEmail(userId, token);
+            if (response.Success)
+                return RedirectToAction();
+
+            return StatusCode(response.Status);
+        }
+
+        [HttpGet]
+        public IActionResult ResetPasswordRequest() => 
+            View(new ResetPasswordRequestViewModel());
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPasswordRequest(ResetPasswordRequestViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var response = await _authService.ResetPasswordRequest(model);
+            if (response.Success) 
+                return RedirectToAction();
+
+            if (response.Status == 404)
+            {
+                ModelState.AddModelError(nameof(model.UsernameOrEmail), "Such a user is not found.");
+                return View(model);
+            }
+
+            return StatusCode(response.Status);
+        }
+
+        [HttpGet]
+        public IActionResult ResetPassword(Guid userId, string token)
+        {
+            var model = new ResetPasswordViewModel 
+            { 
+                UserId = userId, 
+                Token = token 
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ResetPassword(ResetPasswordViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            var response = await _authService.ResetPassword(model);
+            if (response.Success)
+                return RedirectToAction();
+
+            if (response.Status == 400 && response.ValidationErrors.Count() > 0)
+            {
+                foreach (var error in response.ValidationErrors)
+                    ModelState.AddModelError(error.PropertyName, error.ErrorMessage);
+
+                return View(model);
+            }
+
+            return StatusCode(response.Status);
         }
     }
 }
