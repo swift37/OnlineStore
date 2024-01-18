967efe58555ef1008ecfccab3ee3808754da3d31
Swift <swiftx37@gmail.com>
2024-01-18 02:44:51 +0100

    The Login view model updated and UpdateUser view model created.

--- OnlineStore.MVC/Models/LoginViewModel.cs
+++ OnlineStore.MVC/Models/LoginViewModel.cs
@@ -12,6 +12,8 @@ namespace OnlineStore.MVC.Models
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
+        public bool RememberMe { get; set; }
+
         public string? ReturnUrl { get; set; }
     }
 }
---
+++ OnlineStore.MVC/Models/UpdateUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.MVC.Models
+{
+    public class UpdateUserViewModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Phone]
+        public string? PhoneNumber { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+    }
+}
