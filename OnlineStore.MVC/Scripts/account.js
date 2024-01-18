1d2c017104863aa482e8874b2398ffe730101930
Swift <swiftx37@gmail.com>
2024-01-18 21:37:19 +0100

    New js scripts for the Settings page.

--- OnlineStore.MVC/Scripts/account.js
+++ OnlineStore.MVC/Scripts/account.js
@@ -4,11 +4,27 @@ $(document).ready(function () {
         $(this).addClass('active');
 
         let id = $(this).prop('id').slice(1);
+
+        if (id == 0 && $('.form-buttons').hasClass('active'))
+            $(this).closest('.content-box').addClass('changed');
+        else
+            $(this).closest('.content-box').removeClass('changed');
+
         $('.tab-content').children().each((i, el) => {
             if (i == id)
                 $(el).addClass('active');
             else
                 $(el).removeClass('active');
         });
+    });
+
+    $('.tab form input').change(function () {
+        $(this).closest('.content-box').addClass('changed');
+        $('.form-buttons').addClass('active');
+    });
+
+    $('.tab form button').click(function () {
+        $(this).closest('.content-box').removeClass('changed');
+        $(this).parent().removeClass('active');
     });
 });
