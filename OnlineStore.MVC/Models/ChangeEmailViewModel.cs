7c2a2b8d81bf1e281f5808061badb2647f49d9d8
Swift <swiftx37@gmail.com>
2024-01-18 02:45:17 +0100

    The ChangeEmail view model created.

---
+++ OnlineStore.MVC/Models/ChangeEmailViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.MVC.Models
+{
+    public class ChangeEmailViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string NewEmail { get; set; }
+    }
+}
