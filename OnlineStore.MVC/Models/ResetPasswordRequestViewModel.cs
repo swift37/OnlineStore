7bb72cc566b7266e21d97e10a527eabac33e7f64
Swift <swiftx37@gmail.com>
2024-01-18 21:23:06 +0100

    The ResetPasswordRequest view model added.

---
+++ OnlineStore.MVC/Models/ResetPasswordRequestViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OnlineStore.MVC.Models
+{
+    public class ResetPasswordRequestViewModel
+    {
+        [Required]
+        public string UsernameOrEmail { get; set; }
+    }
+}
