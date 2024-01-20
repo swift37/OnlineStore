43457f97ccb33c8a3fd91ba8a3ca30aecd965d7e
Swift <swiftx37@gmail.com>
2024-01-20 22:37:00 +0100

    Updated methods for adding, updating, and removing items from the user's wishlist in the WishlistController.

--- OnlineStore.MVC/Controllers/WishlistController.cs
+++ OnlineStore.MVC/Controllers/WishlistController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OnlineStore.MVC.Attributes;
+using OnlineStore.MVC.Models.Wishlist;
 using OnlineStore.MVC.Services.Interfaces;
 
 namespace OnlineStore.MVC.Controllers
@@ -8,10 +9,9 @@ namespace OnlineStore.MVC.Controllers
     public class WishlistController : Controller
     {
         private readonly IWishlistsService _wishlistsService;
-        private readonly IProductsService _productsService;
 
-        public WishlistController(IWishlistsService wishlistsService, IProductsService productsService) =>
-            (_wishlistsService, _productsService) = (wishlistsService, productsService);
+        public WishlistController(IWishlistsService wishlistsService) =>
+            _wishlistsService = wishlistsService;
 
         [HttpGet]
         [Authorize]
@@ -27,93 +27,64 @@ namespace OnlineStore.MVC.Controllers
         [HttpPost]
         [Authorize]
         [ValidateAjax]
-        public async Task<IActionResult> Add(int productId)
+        public async Task<IActionResult> Add(CreateWishlistItemViewModel model)
         {
-            var getWishlistResponse = await _wishlistsService.GetUserWishlist();
-            if (!getWishlistResponse.Success) 
-                return Json(new 
-                { 
-                    success = false, 
-                    errors = new[] { $"An error occurred. Status code: {getWishlistResponse.Status}" } 
-                });
-
-            var getProductResponse = await _productsService.Get(productId);
-            if (!getProductResponse.Success) 
-                return Json(new 
-                { 
-                    success = false, 
-                    errors = new[] { $"An error occurred. Status code: {getProductResponse.Status}" } 
-                });
-
-            var wishlist = getWishlistResponse.Data;
-            var product = getProductResponse.Data;
-            
-            if (wishlist.Products.Any(product => product.Id == productId))
-                return Json(new 
-                { 
-                    success = false, 
-                    errors = new[] { "Such a product is already on your wish list." } 
-                });
-
-            wishlist.Products.Add(product);
-            wishlist.LastChangeDate = DateTime.Now;
-
-            var updateWishlistReponse = await _wishlistsService.UpdateProducts(wishlist);
-            if (updateWishlistReponse.Success) 
+            var response = await _wishlistsService.AddItem(model);
+            if (response.Success)
                 return Json(new { success = true });
 
-            if (updateWishlistReponse.Status == 400 && updateWishlistReponse.ValidationErrors.Count() > 0)
+            if (response.Status == 400 && response.ValidationErrors.Count() > 0)
                 return Json(new
                 {
                     success = false,
-                    errors = updateWishlistReponse.ValidationErrors
+                    errors = response.ValidationErrors
                         .Select(error => error.ErrorMessage)
                         .ToArray()
                 });
 
-            return Json(new { success = false, errors = new[] { $"An error occurred. Status code: {updateWishlistReponse.Status}" } });
+            return Json(new { success = false, errors = new[] { $"An error occurred. Status code: {response.Status}" } });
         }
 
         [HttpPost]
         [Authorize]
         [ValidateAjax]
-        public async Task<IActionResult> Remove(int productId)
+        public async Task<IActionResult> Update(WishlistItemViewModel model)
         {
-            var getWishlistReponse = await _wishlistsService.GetUserWishlist();
-            if (!getWishlistReponse.Success) 
-                return Json(new 
-                { 
-                    success = false, 
-                    errors = new[] { $"An error occurred. Status code: {getWishlistReponse.Status}" } 
-                });
-
-            var wishlist = getWishlistReponse.Data;
-            var product = wishlist.Products.SingleOrDefault(product => product.Id == productId);
-
-            if (product is null) 
-                return Json(new 
-                { 
-                    success = false, 
-                    errors = new[] { "Such a product is not on your wish list." } 
-                });
-
-            wishlist.Products.Remove(product);
-            wishlist.LastChangeDate = DateTime.Now;
-            
-            var updateWishlistReponse = await _wishlistsService.UpdateProducts(wishlist);
-            if (updateWishlistReponse.Success)
+            var response = await _wishlistsService.UpdateItem(model);
+            if (response.Success)
                 return Json(new { success = true });
 
-            if (updateWishlistReponse.Status == 400 && updateWishlistReponse.ValidationErrors.Count() > 0)
+            if (response.Status == 400 && response.ValidationErrors.Count() > 0)
                 return Json(new
                 {
                     success = false,
-                    errors = updateWishlistReponse.ValidationErrors
+                    errors = response.ValidationErrors
                         .Select(error => error.ErrorMessage)
                         .ToArray()
                 });
 
-            return Json(new { success = false, errors = new[] { $"An error occurred. Status code: {updateWishlistReponse.Status}" } });
+            return Json(new { success = false, errors = new[] { $"An error occurred. Status code: {response.Status}" } });
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAjax]
+        public async Task<IActionResult> Remove(int itemId)
+        {          
+            var response = await _wishlistsService.RemoveItem(itemId);
+            if (response.Success)
+                return Json(new { success = true });
+
+            if (response.Status == 400 && response.ValidationErrors.Count() > 0)
+                return Json(new
+                {
+                    success = false,
+                    errors = response.ValidationErrors
+                        .Select(error => error.ErrorMessage)
+                        .ToArray()
+                });
+
+            return Json(new { success = false, errors = new[] { $"An error occurred. Status code: {response.Status}" } });
         }
     }
 }
