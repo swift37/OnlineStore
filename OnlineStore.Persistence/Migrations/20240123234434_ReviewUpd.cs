3dc0f50891d0711ac1ac7f96e9f5601f3fb3e1c1
Swift <swiftx37@gmail.com>
2024-01-23 22:03:00 +0100

    The database has been updated with the new migration.

---
+++ OnlineStore.Persistence/Migrations/20240123234434_ReviewUpd.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OnlineStore.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ReviewUpd : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Reviews",
+                newName: "Name");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Reviews",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastChangeDate",
+                table: "Reviews",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "OrderId",
+                table: "Reviews",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Reviews_OrderId",
+                table: "Reviews",
+                column: "OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Reviews_Orders_OrderId",
+                table: "Reviews",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Reviews_Orders_OrderId",
+                table: "Reviews");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Reviews_OrderId",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "LastChangeDate",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Reviews");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Reviews",
+                newName: "Title");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "UserId",
+                table: "Reviews",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+        }
+    }
+}
