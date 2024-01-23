ecd7da2ab2c5210fafc8675a21ee153caafcfa91
Swift <swiftx37@gmail.com>
2024-01-23 20:54:18 +0100

    Added the IsEmpty field to the ReviewsPageViewModel

--- OnlineStore.MVC/Models/ReviewsPageViewModel.cs
+++ OnlineStore.MVC/Models/ReviewsPageViewModel.cs
@@ -8,5 +8,7 @@ namespace OnlineStore.MVC.Models
         public IEnumerable<OrderViewModel> OrdersAwaitingReview { get; set; } = Enumerable.Empty<OrderViewModel>();
 
         public IEnumerable<ReviewViewModel> Reviews { get; set; } = Enumerable.Empty<ReviewViewModel>();
+
+        public bool IsEmpty => !OrdersAwaitingReview.Any() && !Reviews.Any();
     }
 }
