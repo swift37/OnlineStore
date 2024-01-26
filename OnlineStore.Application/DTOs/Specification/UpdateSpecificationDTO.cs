a916630590bee2ff1796f0b3b7617ce469b22f2b
Swift <swiftx37@gmail.com>
2024-01-26 21:59:26 +0100

    The UpdateSpecification DTO has been updated.

--- OnlineStore.Application/DTOs/Specification/UpdateSpecificationDTO.cs
+++ OnlineStore.Application/DTOs/Specification/UpdateSpecificationDTO.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using OnlineStore.Application.DTOs.Base;
-using OnlineStore.Application.DTOs.Product;
 using OnlineStore.Application.Mapping;
 
 namespace OnlineStore.Application.DTOs.Specification
@@ -10,8 +9,6 @@ namespace OnlineStore.Application.DTOs.Specification
         public int SpecificationTypeId { get; set; }
 
         public string? Value { get; set; }
-
-        public ICollection<ProductDTO> Products { get; set; } = new HashSet<ProductDTO>();
 
         public void Mapping(Profile profile) =>
             profile.CreateMap<Domain.Entities.Specification, UpdateSpecificationDTO>().ReverseMap();
