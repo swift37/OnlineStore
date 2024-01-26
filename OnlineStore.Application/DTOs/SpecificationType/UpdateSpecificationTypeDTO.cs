bccaeac37f10de148b9b63424b12aee1e131f2b1
Swift <swiftx37@gmail.com>
2024-01-26 23:03:00 +0100

    The UpdateSpecificationType DTO has been updated.

--- OnlineStore.Application/DTOs/SpecificationType/UpdateSpecificationTypeDTO.cs
+++ OnlineStore.Application/DTOs/SpecificationType/UpdateSpecificationTypeDTO.cs
@@ -1,6 +1,5 @@
 ﻿using AutoMapper;
 using OnlineStore.Application.DTOs.Base;
-using OnlineStore.Application.DTOs.Specification;
 using OnlineStore.Application.Mapping;
 
 namespace OnlineStore.Application.DTOs.SpecificationType
@@ -12,8 +11,6 @@ namespace OnlineStore.Application.DTOs.SpecificationType
         public string? DisplayName { get; set; }
 
         public bool IsMain { get; set; }
-
-        public ICollection<UpdateSpecificationDTO> Values { get; set; } = new HashSet<UpdateSpecificationDTO>();
 
         public void Mapping(Profile profile) =>
             profile.CreateMap<Domain.Entities.SpecificationType, UpdateSpecificationTypeDTO>().ReverseMap();
