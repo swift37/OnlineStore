5e724a6b3e6148db2fb5eda13daebb0471546cf8
Swift <swiftx37@gmail.com>
2024-01-29 23:48:13 +0100

    View models for NestedMenuItem have been created.

---
+++ OnlineStore.MVC/Models/NestedMenuItem/NestedMenuItemViewModel.cs
@@ -0,0 +1,20 @@
+﻿using OnlineStore.MVC.Models.Category;
+using OnlineStore.MVC.Models.MenuItem;
+
+namespace OnlineStore.MVC.Models.NestedMenuItem
+{
+    public class NestedMenuItemViewModel
+    {
+        public int Id { get; set; }
+
+        public string? NavigationLabel { get; set; }
+
+        public int ParentId { get; set; }
+
+        public MenuItemViewModel? Parent { get; set; }
+
+        public ICollection<CategoryViewModel> Categories { get; set; } = new HashSet<CategoryViewModel>();
+
+        public bool HasTwoColumns { get; set; }
+    }
+}
