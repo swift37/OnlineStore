d9feea062aa62a636691f8c4a482d8a269cfa2e1
Swift <swiftx37@gmail.com>
2024-01-31 23:59:55 +0100

    Created the ApiAuthenticationException model.

---
+++ OnlineStore.MVC/Models/Exceptions/ApiAuthenticationException.cs
@@ -0,0 +1,18 @@
+﻿namespace OnlineStore.MVC.Models.Exceptions
+{
+    public class ApiAuthenticationException : Exception
+    {
+        public string? SourceUrl { get; set; }
+
+        public ApiAuthenticationException() : base() { }
+
+        public ApiAuthenticationException(string? sourceUrl) : base() => 
+            SourceUrl = sourceUrl;
+
+        public ApiAuthenticationException(string? sourceUrl, string message) : base(message) =>
+            SourceUrl = sourceUrl;
+
+        public ApiAuthenticationException(string? sourceUrl, string message, Exception innerException) 
+            : base(message, innerException) => SourceUrl = sourceUrl;
+    }
+}
