e44638cbd1c6a4f178c6e75f22ab54148e76d1c6
Swift <swiftx37@gmail.com>
2024-02-05 20:30:57 +0100

    Created the ReviewsList view component.

---
+++ OnlineStore.MVC/ViewComponents/ReviewsListViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.MVC.Services.Interfaces;
+
+namespace OnlineStore.MVC.ViewComponents
+{
+    public class ReviewsListViewComponent : ViewComponent
+    {
+        private readonly IReviewsService _reviewsService;
+
+        public ReviewsListViewComponent(IReviewsService reviewsService) =>
+            _reviewsService = reviewsService;
+
+        public async Task<IViewComponentResult> InvokeAsync(int productId)
+        {
+            var response = await _reviewsService.GetReviewsByProduct(productId);
+            var model = response.Data;
+
+            return View(model);
+        }
+    }
+}
