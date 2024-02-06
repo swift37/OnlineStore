df7a295ac9f6db11e4278a013cdff31c6f88fb20
Swift <swiftx37@gmail.com>
2024-02-06 15:50:18 +0100

    The WishlistButton view component has been created.

---
+++ OnlineStore.MVC/ViewComponents/WishlistButtonViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.MVC.Services.Interfaces;
+
+namespace OnlineStore.MVC.ViewComponents
+{
+    public class WishlistButtonViewComponent : ViewComponent
+    {
+        private readonly IWishlistsService _wishlistsService;
+
+        public WishlistButtonViewComponent(IWishlistsService wishlistsService) => 
+            _wishlistsService = wishlistsService;
+
+        public async Task<IViewComponentResult> InvokeAsync(int productId)
+        {
+            var response = await _wishlistsService.CheckProductPresence(productId);
+            var result = response.Data;
+
+            if (result)
+                ViewBag.ItemId = (await _wishlistsService.GetItemId(productId)).Data;
+            
+            ViewBag.ProductId = productId;
+
+            return View();
+        }
+    }
+}
