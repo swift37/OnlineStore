8151b7ff8ac39abf5221f8a3f5f5c595edb36b4a
Swift <swiftx37@gmail.com>
2024-02-09 23:01:00 +0100

    The Specification entity configuration has been updated.

--- OnlineStore.Persistence/EntityTypeConfigurations/SpecificationConfiguration.cs
+++ OnlineStore.Persistence/EntityTypeConfigurations/SpecificationConfiguration.cs
@@ -10,7 +10,8 @@ namespace OnlineStore.Persistence.EntityTypeConfigurations
         public override void Configure(EntityTypeBuilder<Specification> builder)
         {
             base.Configure(builder);
-            builder.Property(specificationType => specificationType.Value).HasMaxLength(32);
+            builder.Property(specification => specification.Value).HasMaxLength(32);
+            builder.Ignore(specification => specification.ProductsCount);
             builder
                 .HasOne(specification => specification.SpecificationType)
                 .WithMany(specificationType => specificationType.Values)
