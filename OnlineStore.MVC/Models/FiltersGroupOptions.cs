cda4014eb70230a6cf8005636344c02401975587
Swift <swiftx37@gmail.com>
2024-02-10 21:06:00 +0100

    Created the FiltersGroupOptions model in the MVC project.

---
+++ OnlineStore.MVC/Models/FiltersGroupOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace OnlineStore.MVC.Models
+{
+    public class FiltersGroupOptions
+    {
+        public int CategoryId { get; set; }
+
+        public IDictionary<int, ICollection<int>> AppliedFilters { get; set; } = new Dictionary<int, ICollection<int>>();
+    }
+}
