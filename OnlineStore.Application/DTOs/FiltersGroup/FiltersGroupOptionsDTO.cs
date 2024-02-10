9ee4651f7a32bfdc5e1b52c7ad5d04e06429377b
Swift <swiftx37@gmail.com>
2024-02-10 21:01:00 +0100

    The FiltersGroupOptions DTO with mapping has been created.

---
+++ OnlineStore.Application/DTOs/FiltersGroup/FiltersGroupOptionsDTO.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using OnlineStore.Application.Mapping;
+using OnlineStore.Domain;
+
+namespace OnlineStore.Application.DTOs.FiltersGroup
+{
+    public class FiltersGroupOptionsDTO : IMapWith<FiltersGroupOptions>
+    {
+        public int CategoryId { get; set; }
+
+        public IDictionary<int, ICollection<int>> AppliedFilters { get; set; } = new Dictionary<int, ICollection<int>>();
+
+        public void Mapping(Profile profile) =>
+            profile.CreateMap<FiltersGroupOptions, FiltersGroupOptionsDTO>().ReverseMap();
+    }
+}
