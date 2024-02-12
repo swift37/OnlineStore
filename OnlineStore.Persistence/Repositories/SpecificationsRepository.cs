42c8219b2c280718d004aaf989dbe1ccfa5fc0d9
Swift <swiftx37@gmail.com>
2024-02-12 20:07:38 +0100

    Implementation of the GetMany method in the Specificaitons repository.

--- OnlineStore.Persistence/Repositories/SpecificationsRepository.cs
+++ OnlineStore.Persistence/Repositories/SpecificationsRepository.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OnlineStore.Application.Exeptions;
 using OnlineStore.Application.Interfaces;
+using OnlineStore.Application.Interfaces.Repositories;
 using OnlineStore.DAL.Repositories;
 using OnlineStore.Domain.Entities;
 
 namespace OnlineStore.Persistence.Repositories
 {
-    public class SpecificationsRepository : Repository<Specification>
+    public class SpecificationsRepository : Repository<Specification>, ISpecificationsRepository
     {
         protected override IQueryable<Specification> Entities => base.Entities
             .Include(specification => specification.SpecificationType);
 
         public SpecificationsRepository(IApplicationDbContext context) : base(context) { }
+
+        public async Task<IEnumerable<Specification>> GetManyAsync(
+            IEnumerable<int> ids,
+            CancellationToken cancellation = default) => await Entities
+            .Where(s => ids.Contains(s.Id))
+            .ToArrayAsync()
+            .ConfigureAwait(false)
+            ?? throw new NotFoundException(nameof(Specification), ids);
     }
 }
