ee57061ce676eae27fcdd3442625bb63d9e69b4d
Swift <swiftx37@gmail.com>
2024-02-12 20:05:38 +0100

    Validation for the options models.

---
+++ OnlineStore.Application/DTOs/FiltersGroup/Validation/FiltersGroupOptionsValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using OnlineStore.Domain;
+
+namespace OnlineStore.Application.DTOs.FiltersGroup.Validation
+{
+    public class FiltersGroupOptionsValidator : AbstractValidator<FiltersGroupOptions>
+    {
+        public FiltersGroupOptionsValidator()
+        {
+            RuleFor(opt => opt.CategoryId)
+                .GreaterThan(0);
+        }
+    }
+}
---
+++ OnlineStore.Application/DTOs/SpecificationType/Validation/SpecificationTypeOptionsValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using OnlineStore.Domain;
+
+namespace OnlineStore.Application.DTOs.SpecificationType.Validation
+{
+    public class SpecificationTypeOptionsValidator : AbstractValidator<SpecificationTypeOptions>
+    {
+        public SpecificationTypeOptionsValidator()
+        {
+            RuleFor(opt => opt.Id)
+                .GreaterThan(0);
+        }
+    }
+}
