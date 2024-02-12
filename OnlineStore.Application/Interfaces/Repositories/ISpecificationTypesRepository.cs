365685a27d7f63c05d50d9fc69ab842d360c32c4
Swift <swiftx37@gmail.com>
2024-02-12 20:06:33 +0100

    Created interfaces for the Specification and SpecificationType repositories.

---
+++ OnlineStore.Application/Interfaces/Repositories/ISpecificationTypesRepository.cs
@@ -0,0 +1,12 @@
+﻿using OnlineStore.Domain;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Application.Interfaces.Repositories
+{
+    public interface ISpecificationTypesRepository : IRepository<SpecificationType>
+    {
+        Task<SpecificationType> GetAsync(
+            SpecificationTypeOptions options,
+            CancellationToken cancellation = default);
+    }
+}
---
+++ OnlineStore.Application/Interfaces/Repositories/ISpecificationsRepository.cs
@@ -0,0 +1,11 @@
+﻿using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Application.Interfaces.Repositories
+{
+    public interface ISpecificationsRepository : IRepository<Specification>
+    {
+        Task<IEnumerable<Specification>> GetManyAsync(
+            IEnumerable<int> ids,
+            CancellationToken cancellation = default);
+    }
+}
