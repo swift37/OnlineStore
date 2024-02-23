f892f2159084d50a54ce16af072c531fbe29939b
Swift <swiftx37@gmail.com>
2024-02-23 22:35:47 +0100

    New view component to display the list of reviews on the Account/Reviews page.

---
+++ OnlineStore.MVC/ViewComponents/UserReviewsListViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.MVC.Services.Interfaces;
+
+namespace OnlineStore.MVC.ViewComponents
+{
+    public class UserReviewsListViewComponent : ViewComponent
+    {
+        private readonly IReviewsService _reviewsService;
+
+        public UserReviewsListViewComponent(IReviewsService reviewsService) =>
+            _reviewsService = reviewsService;
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var response = await _reviewsService.GetUserReviews();
+            var model = response.Data;
+            return View(model);
+        }
+    }
+}
