c8b850f91765cbbb620aed48865d5a9d21d19468
Swift <swiftx37@gmail.com>
2024-06-17 22:04:33 +0200

    Entity type configurations for new domain entities.

---
+++ OnlineStore.Persistence/EntityTypeConfigurations/PaymentMethodConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineStore.DAL.EntityTypeConfigurations.Base;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Persistence.EntityTypeConfigurations
+{
+    public class PaymentMethodConfiguration : BaseConfiguration<PaymentMethod>
+    {
+        public override void Configure(EntityTypeBuilder<PaymentMethod> builder)
+        {
+            base.Configure(builder);
+            builder.HasIndex(paymentMethod => paymentMethod.Name).IsUnique();
+            builder.Property(paymentMethod => paymentMethod.Name).HasMaxLength(32);
+            builder.Property(paymentMethod => paymentMethod.DisplayName).HasMaxLength(32);
+        }
+    }
+}
---
+++ OnlineStore.Persistence/EntityTypeConfigurations/ShippingMethodConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using OnlineStore.DAL.EntityTypeConfigurations.Base;
+using OnlineStore.Domain.Entities;
+
+namespace OnlineStore.Persistence.EntityTypeConfigurations
+{
+    public class ShippingMethodConfiguration : BaseConfiguration<ShippingMethod>
+    {
+        public override void Configure(EntityTypeBuilder<ShippingMethod> builder)
+        {
+            base.Configure(builder);
+            builder.HasIndex(shippingMethod => shippingMethod.Name).IsUnique();
+            builder.Property(shippingMethod => shippingMethod.Name).HasMaxLength(32);
+            builder.Property(shippingMethod => shippingMethod.DisplayName).HasMaxLength(32);
+            builder.Property(shippingMethod => shippingMethod.Price).HasColumnType("decimal(18,2)");
+        }
+    }
+}
