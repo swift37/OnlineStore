e908e07942025b221908cdb326ef056dc6339174
Swift <swiftx37@gmail.com>
2024-06-18 00:54:30 +0200

    The ShippingMethods view component has been created.

---
+++ OnlineStore.MVC/ViewComponents/ShippingMethodsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.MVC.Services.Interfaces;
+
+namespace OnlineStore.MVC.ViewComponents
+{
+    public class ShippingMethodsViewComponent : ViewComponent
+    {
+        private readonly IShippingMethodsService _shippingMethodsService;
+
+        public ShippingMethodsViewComponent(IShippingMethodsService shippingMethodsService) =>
+            _shippingMethodsService = shippingMethodsService;
+
+        public async Task<IViewComponentResult> InvokeAsync(int productId, string? text)
+        {
+            var response = await _shippingMethodsService.GetAll();
+            var result = response.Data.Where(method => method.IsAvailable);
+
+            return View(result);
+        }
+    }
+}
