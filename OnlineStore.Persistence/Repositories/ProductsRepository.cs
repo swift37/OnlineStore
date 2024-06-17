abb57743289eee66cb54080240bcfe7c4ebd514c
Swift <swiftx37@gmail.com>
2024-06-17 22:07:33 +0200

    New methods have been implemented in the ProductsRepository.

--- OnlineStore.Persistence/Repositories/ProductsRepository.cs
+++ OnlineStore.Persistence/Repositories/ProductsRepository.cs
@@ -4,6 +4,7 @@ using OnlineStore.Application.Interfaces;
 using OnlineStore.Application.Interfaces.Repositories;
 using OnlineStore.Domain;
 using OnlineStore.Domain.Entities;
+using OnlineStore.Domain.Enums;
 
 namespace OnlineStore.DAL.Repositories
 {
@@ -11,8 +12,10 @@ namespace OnlineStore.DAL.Repositories
     {
         protected override IQueryable<Product> Entities => base.Entities
             .Include(product => product.Category)
+            .Include(product => product.Tags)
             .Include(product => product.Specifications)
-                .ThenInclude(specification => specification.SpecificationType);
+                .ThenInclude(specification => specification.SpecificationType)
+            .AsNoTracking();
 
         public ProductsRepository(IApplicationDbContext context) : base(context) { }
 
@@ -47,6 +50,7 @@ namespace OnlineStore.DAL.Repositories
                 / options.ItemsPerPage;
 
             var productsCollection = await query
+                .OrderBy(p => p.Availability)
                 .Skip((options.PageNumber - 1) * options.ItemsPerPage)
                 .Take(options.ItemsPerPage)
                 .Include(p => p.Category)
@@ -71,6 +75,34 @@ namespace OnlineStore.DAL.Repositories
             .Where(p => p.CategoryId == categoryId && p.Specifications
                 .Any(s => s.Id == specificationId))
             .CountAsync(cancellation)
+            .ConfigureAwait(false);
+
+        public async Task<IEnumerable<Product>> GetAllByTagAsync(
+            int tagId,
+            CancellationToken cancellation = default) => await Entities
+            .Where(p => p.Tags.Any(tag => tag.Id == tagId))
+            .ToArrayAsync()
+            .ConfigureAwait(false);
+
+        public async Task<IEnumerable<Product>> GetAllByTagAsync(
+            string tagName,
+            CancellationToken cancellation = default) => await Entities
+            .Where(p => p.Tags.Any(tag => tag.Name == tagName))
+            .ToArrayAsync()
+            .ConfigureAwait(false);
+
+        public async Task<IEnumerable<Product>> GetAllByAvailabilityAsync(
+            ProductAvailability productAvailability,
+            CancellationToken cancellation = default) => await Entities
+            .Where(p => p.Availability == productAvailability)
+            .ToArrayAsync()
+            .ConfigureAwait(false);
+
+        public async Task<IEnumerable<Product>> GetAllByStatusAsync(
+            ProductStatus productStatus,
+            CancellationToken cancellation = default) => await Entities
+            .Where(p => p.Status == productStatus)
+            .ToArrayAsync()
             .ConfigureAwait(false);
     }
 }
