27349c6ed3ac2d9cf0af0ebc135318d5c4fad6bc
Swift <swiftx37@gmail.com>
2024-06-17 21:56:20 +0200

    ProductTag domain entity has been added.

---
+++ OnlineStore.Domain/Entities/ProductTag.cs
@@ -0,0 +1,13 @@
+﻿using OnlineStore.Domain.Base;
+
+namespace OnlineStore.Domain.Entities
+{
+    public class ProductTag : NamedEntity
+    {
+        public string? DisplayName { get; set; }
+
+        public string? ColorHex { get; set; }
+
+        public ICollection<Product> Products { get; set; } = new HashSet<Product>();
+    }
+}
