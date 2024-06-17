5243034c0420a6b4be0d9ce80060e8ba9b0aa096
Swift <swiftx37@gmail.com>
2024-06-18 00:54:10 +0200

    The PaymentMethods view component has been created.

---
+++ OnlineStore.MVC/ViewComponents/PaymentMethodsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.MVC.Services.Interfaces;
+
+namespace OnlineStore.MVC.ViewComponents
+{
+    public class PaymentMethodsViewComponent : ViewComponent
+    {
+        private readonly IPaymentMethodsService _paymentMethodsService;
+
+        public PaymentMethodsViewComponent(IPaymentMethodsService paymentMethodsService) =>
+            _paymentMethodsService = paymentMethodsService;
+
+        public async Task<IViewComponentResult> InvokeAsync(int productId, string? text)
+        {
+            var response = await _paymentMethodsService.GetAll();
+            var result = response.Data.Where(method => method.IsAvailable);
+
+            return View(result);
+        }
+    }
+}
