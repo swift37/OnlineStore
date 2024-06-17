8b551194ac63ed25368c3e7193041540cc632c98
Swift <swiftx37@gmail.com>
2024-06-18 00:53:04 +0200

    The FeaturedProducts view component has been created.

---
+++ OnlineStore.MVC/ViewComponents/FeaturedProductsViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineStore.MVC.Services.Interfaces;
+
+namespace OnlineStore.MVC.ViewComponents
+{
+    public class FeaturedProductsViewComponent : ViewComponent
+    {
+        private readonly IProductsService _productsService;
+
+        public FeaturedProductsViewComponent(IProductsService productsService) =>
+            _productsService = productsService;
+
+        public async Task<IViewComponentResult> InvokeAsync(int productId, string? text)
+        {
+            var response = await _productsService.GetAllByTag("featured");
+            var result = response.Data;
+
+            if (!result.Any())
+                return Content(string.Empty);
+
+            return View(result);
+        }
+    }
+}
