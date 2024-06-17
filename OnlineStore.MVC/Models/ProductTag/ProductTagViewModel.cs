26d8368c6e74db0b7121674233ef7f5689be2cdf
Swift <swiftx37@gmail.com>
2024-06-18 00:45:58 +0200

    View models for new entities have been created.

---
+++ OnlineStore.MVC/Models/ProductTag/ProductTagViewModel.cs
@@ -0,0 +1,17 @@
+﻿using OnlineStore.MVC.Models.Product;
+
+namespace OnlineStore.MVC.Models.ProductTag
+{
+    public class ProductTagViewModel
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? DisplayName { get; set; }
+
+        public string? ColorHex { get; set; }
+
+        public ICollection<ProductViewModel> Products { get; set; } = new HashSet<ProductViewModel>();
+    }
+}
---
+++ OnlineStore.MVC/Models/ShippingMethod/ShippingMethodViewModel.cs
@@ -0,0 +1,21 @@
+﻿using OnlineStore.MVC.Models.Order;
+
+namespace OnlineStore.MVC.Models.ShippingMethod
+{
+    public class ShippingMethodViewModel
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? DisplayName { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string? Image { get; set; }
+
+        public ICollection<OrderViewModel> Orders { get; set; } = new HashSet<OrderViewModel>();
+
+        public bool IsAvailable { get; set; }
+    }
+}
